16b200c4a39679583d7074fd5d1e3a1a30afc276
mleigh <michael.leigh@mu.ie>
2018-07-19 18:10:11 +0100

    Front-end Updates
    Updated the landing page, navigation and accordions. Created mixins for navs and accordions

---
+++ public/javascripts/accordion.js
@@ -0,0 +1,9 @@
+var $title = $(".accordion__heading");
+var $content = $(".accordion__content");
+
+$title.click(function () {
+  $(this).parent().toggleClass('accordion__item--active');
+  // make other accordion__items hidden
+  // $(this).parent().siblings().removeClass('accordion__item--active');
+  return false;
+});
