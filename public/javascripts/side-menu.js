4d1cffdbc63861274935e01b1ec4c3a0bf29385b
mleigh <michael.leigh@mu.ie>
2018-07-23 14:34:45 +0100

    Updates Layout and scroll button
    Updates to side menu, stories page, charts.

--- public/javascripts/side-menu.js
+++ public/javascripts/side-menu.js
@@ -1,4 +1,7 @@
+// simple on click to toggle menu
 $('.side-menu__toggler').on('click', function () {
+    // toggle class for side menu
     $('.side-menu').toggleClass('side-menu--open');
+    // toggle class for toggler button
     $(this).toggleClass('side-menu__toggler--closed');
 });    
