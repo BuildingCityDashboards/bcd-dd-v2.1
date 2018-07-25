e07ebf2bd1653e4e77c23bd8ca8f953be8e72157
mleigh <michael.leigh@mu.ie>
2018-07-25 09:43:50 +0100

    Fixes and Updates landing Page
    Small updates to menu, adjusted layout and stories page

--- public/javascripts/side-menu.js
+++ public/javascripts/side-menu.js
@@ -2,6 +2,8 @@
 $('.side-menu__toggler').on('click', function () {
     // toggle class for side menu
     $('.side-menu').toggleClass('side-menu--open');
+    // bcd-container
+    $('.bcd--container').toggleClass('bcd--container--push');
     // toggle class for toggler button
     $(this).toggleClass('side-menu__toggler--closed');
 });    
