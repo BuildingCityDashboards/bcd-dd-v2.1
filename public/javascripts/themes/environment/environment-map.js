8c2e11ed2e2b568cfd4f3c6c37002b64db541bc6
Liam <Liam@192.168.1.15>
2018-10-01 10:39:08 +0100

    Streamline Query for Getting Around with single realtime map

--- public/javascripts/themes/environment/environment-map.js
+++ public/javascripts/themes/environment/environment-map.js
@@ -1,21 +1,4 @@
 let popupTime = d3.timeFormat("%a %B %d, %H:%M");
-//  1. declare map variables and initialise base map
-let map = new L.Map('chart-environment-map');
-let dubLat = 53.3498;
-let dubLng = -6.5;
-let min_zoom = 8, max_zoom = 18;
-let zoom = 10;
-
-// tile layer with correct attribution
-let osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-let osmUrl_BW = 'http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png';
-let osmUrl_Hot = 'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png';
-let stamenTonerUrl = 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png';
-let stamenTerrainUrl = 'https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png';
-let stamenTonerUrl_Lite = 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png';
-let osmAttrib = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-let osmAttrib_Hot = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>';
-let stamenTonerAttrib = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
 let osm = new L.TileLayer(stamenTerrainUrl, {
     minZoom: min_zoom,
     maxZoom: max_zoom,
