eeb3b8f18a0b9f60dac080240eb9878da5394665
Liam <Liam@192.168.1.15>
2018-10-07 20:35:42 +0100

    Start Tools redesign

--- routes/tools.js
+++ routes/tools.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET page. */
 router.get('/', function(req, res, next) {
-//  res.render('tools', { title: 'Tools Page' });
-res.redirect('/tools/planning');
+  res.render('tools', { title: 'Tools Page' });
+//res.render('/tools/planning');
 });
 
 router.get('/planning', function(req, res, next) {
-  res.render('tools_planning', { title: 'Tools: Planning' });
+  res.render('tools-planning', { title: 'Tools: Planning' });
 });
 
 module.exports = router;
