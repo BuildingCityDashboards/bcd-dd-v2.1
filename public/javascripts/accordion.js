ec05496136f94921dde73675b204ef352934af4b
LiamOSullivan <lmosulli@tcd.ie>
2019-09-03 13:18:10 +0100

    Style and add content for portal

--- public/javascripts/accordion.js
+++ public/javascripts/accordion.js
@@ -2,9 +2,15 @@ let title = $(".accordion__heading");
 let question = $(".according__question");
 let content = $(".accordion__content");
 
+
 title.click(function() {
+  $(this).toggleClass('accordion__heading--active');
   $(this).parent().toggleClass('accordion__item--active');
+
   // make other accordion__items hidden
+  $(this).parent().siblings().children(".accordion__heading").removeClass('accordion__heading--active');
   $(this).parent().siblings().removeClass('accordion__item--active');
+
+
   return false;
 });
