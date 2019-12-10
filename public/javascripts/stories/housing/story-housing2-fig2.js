1fae0743151746d0621b71783527378f684a2fc5
LiamOSullivan <lmosulli@tcd.ie>
2019-12-10 10:10:38 +0000

    Fix yaxis range fig 8

--- public/javascripts/stories/housing/story-housing2-fig2.js
+++ public/javascripts/stories/housing/story-housing2-fig2.js
@@ -188,6 +188,7 @@ d3.csv(srcPathFig2)
         {
           args: [{
               'visible': [false, false, true, true, true, true, true, true, true, true
+
                 // false, false, false, false, false, false, false, false,
                 // true, true, true, true, true, true, true, true
               ]
