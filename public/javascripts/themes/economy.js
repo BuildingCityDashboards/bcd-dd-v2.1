1509e0c593c2489732e35b230bbcadd360bc9cdf
LiamOSullivan <lmosulli@tcd.ie>
2020-01-17 13:58:09 +0000

    Restructure common functions for themes page

--- public/javascripts/themes/economy.js
+++ public/javascripts/themes/economy.js
@@ -1,34 +1,32 @@
-let employmentLine, unemploymentLine, employmentStack, unemploymentStack;
-let annual = "../data/Economy/annualemploymentchanges.csv",
-  QNQ22 = "../data/Economy/QNQ22_2.csv",
-  pageSize = 12;
+let employmentLine, unemploymentLine, employmentStack, unemploymentStack
+let annual = '../data/Economy/annualemploymentchanges.csv',
+  QNQ22 = '../data/Economy/QNQ22_2.csv',
+  pageSize = 12
 
-
-
-/*** This QNQ22 employment and unemployment Charts ***/
+/** * This QNQ22 employment and unemployment Charts ***/
 Promise.all([
-    d3.csv(annual),
-    d3.csv(QNQ22)
+  d3.csv(annual),
+  d3.csv(QNQ22)
 
-  ]).then(datafiles => {
-    const QNQ22 = datafiles[1],
-      annual = datafiles[0],
-      keys = QNQ22.columns.slice(3), // 0-2 is date, quarter, region
-      groupBy = "region",
-      keysA = annual.columns.slice(2),
-      test = d3Nest(QNQ22, "date"); // annual rate keys
+]).then(datafiles => {
+  const QNQ22 = datafiles[1],
+    annual = datafiles[0],
+    keys = QNQ22.columns.slice(3), // 0-2 is date, quarter, region
+    groupBy = 'region',
+    keysA = annual.columns.slice(2),
+    test = d3Nest(QNQ22, 'date') // annual rate keys
 
     // coerce values and parse dates
-    coerceNum(QNQ22, keys);
-    coerceNum(annual, keysA);
-    parseQuarter(QNQ22, "quarter");
-    parseYearDates(annual, "date");
+  coerceNum(QNQ22, keys)
+  coerceNum(annual, keysA)
+  parseQuarter(QNQ22, 'quarter')
+  parseYearDates(annual, 'date')
     // d3.select('#economy').selectAll(".chart-holder").style("background-image", "none");
     // d3.select('#').selectAll(".chart-holder").style("background-color","#f8f8f8");
     // d3.selectAll(".chart-holder_PH").attr("class", "chart-holder");
     // d3.select('#chart-healthlevels').selectAll(".chart-holder").style("background-image", "none");
 
-    /*d3.select("#chart-employment").style("background-image", "none");
+    /* d3.select("#chart-employment").style("background-image", "none");
     d3.select("#chart-emp-rate").style("background-image", "none");
     d3.select("#chart-unemp-rate").style("background-image", "none");
     d3.select("#chart-unemployment").style("background-image", "none");
@@ -42,10 +40,10 @@ Promise.all([
     d3.select("#chart-watercons").style("background-image", "none");
     d3.select("#chart-riverqualities").style("background-image", "none");
     d3.select("#chart-greenflags").style("background-image", "none");
-    d3.select("#chart-localagendas").style("background-image", "none");*/
+    d3.select("#chart-localagendas").style("background-image", "none"); */
     //
 
-    /*d3.select("#").style("background-image", "none");
+    /* d3.select("#").style("background-image", "none");
     d3.select("#").style("background-image", "none");
     d3.select("#").style("background-image", "none");
     d3.select("#").style("background-image", "none");
@@ -57,134 +55,127 @@ Promise.all([
     d3.select("#").style("background-image", "none");
     d3.select("#").style("background-image", "none");
     d3.select("#").style("background-image", "none");
-    d3.select("#").style("background-image", "none");*/
+    d3.select("#").style("background-image", "none"); */
     //
 
-
-
-
-
-
-    const emp = keys[0],
-      unemp = QNQ22.columns[4],
-      fData = filterbyDate(QNQ22, "date", "Jan 01  2001"),
-      aNest = d3Nest(annual, groupBy),
-      unempData = stackNest(fData, "label", "region", unemp),
-      empData = stackNest(fData, "label", "region", emp),
-      grouping = ["Dublin", "Rest of Ireland"],
-      unempContent = {
-        e: "#chart-unemp-rate",
-        d: aNest,
-        xV: "date",
-        yV: keysA[1],
-        tX: "Years",
-        tY: "%",
-        ySF: "percentage",
-      },
-      empContent = {
-        e: "#chart-emp-rate",
-        d: aNest,
-        k: "region",
-        xV: "date",
-        yV: keysA[0],
-        tX: "Years",
-        tY: "%",
-        ySF: "percentage",
-      },
-      unempCStack = {
-        e: "#chart-unemployment",
-        d: unempData,
-        ks: grouping,
-        xV: "date",
-        tX: "Quarters",
-        tY: "Thousands",
-        ySF: "millions",
-      },
-      empCStack = {
-        e: "#chart-employment",
-        d: empData,
-        ks: grouping,
-        xV: "date",
-        tX: "Quarters",
-        tY: "Thousands",
-        ySF: "millions",
-      };
-    employmentStack = new StackedAreaChart(empCStack);
-    employmentStack.tickNumber = 12;
+  const emp = keys[0],
+    unemp = QNQ22.columns[4],
+    fData = filterbyDate(QNQ22, 'date', 'Jan 01  2001'),
+    aNest = d3Nest(annual, groupBy),
+    unempData = stackNest(fData, 'label', 'region', unemp),
+    empData = stackNest(fData, 'label', 'region', emp),
+    grouping = ['Dublin', 'Rest of Ireland'],
+    unempContent = {
+      e: '#chart-unemp-rate',
+      d: aNest,
+      xV: 'date',
+      yV: keysA[1],
+      tX: 'Years',
+      tY: '%',
+      ySF: 'percentage'
+    },
+    empContent = {
+      e: '#chart-emp-rate',
+      d: aNest,
+      k: 'region',
+      xV: 'date',
+      yV: keysA[0],
+      tX: 'Years',
+      tY: '%',
+      ySF: 'percentage'
+    },
+    unempCStack = {
+      e: '#chart-unemployment',
+      d: unempData,
+      ks: grouping,
+      xV: 'date',
+      tX: 'Quarters',
+      tY: 'Thousands',
+      ySF: 'millions'
+    },
+    empCStack = {
+      e: '#chart-employment',
+      d: empData,
+      ks: grouping,
+      xV: 'date',
+      tX: 'Quarters',
+      tY: 'Thousands',
+      ySF: 'millions'
+    }
+  employmentStack = new StackedAreaChart(empCStack)
+  employmentStack.tickNumber = 12
     //   employmentStack.pagination(empData, "#chart-employment", 24, 3, "year", "Thousands - Quarter:");
     //   employmentStack.getData(empData);
-    employmentStack.drawChart();
-    employmentStack.addTooltip("Thousands - Quarter:", "thousands", "label");
+  employmentStack.drawChart()
+  employmentStack.addTooltip('Thousands - Quarter:', 'thousands', 'label')
 
-    employmentLine = new MultiLineChart(empContent);
-    employmentLine.tickNumber = 12;
-    employmentLine.drawChart();
-    employmentLine.addTooltip("Employment Annual % Change - ", "percentage2", "label");
-    employmentLine.hideRate(true); //hides the rate column in the tooltip when the % change chart is shown
+  employmentLine = new MultiLineChart(empContent)
+  employmentLine.tickNumber = 12
+  employmentLine.drawChart()
+  employmentLine.addTooltip('Employment Annual % Change - ', 'percentage2', 'label')
+  employmentLine.hideRate(true) // hides the rate column in the tooltip when the % change chart is shown
 
-    unemploymentStack = new StackedAreaChart(unempCStack);
-    unemploymentStack.tickNumber = 12;
+  unemploymentStack = new StackedAreaChart(unempCStack)
+  unemploymentStack.tickNumber = 12
     //   unemploymentStack.getData(unempData);
-    unemploymentStack.drawChart();
-    unemploymentStack.addTooltip("Thousands - Quarter:", "thousands", "label");
+  unemploymentStack.drawChart()
+  unemploymentStack.addTooltip('Thousands - Quarter:', 'thousands', 'label')
     //   employmentLine.createScales();
 
-    unemploymentLine = new MultiLineChart(unempContent);
-    unemploymentLine.tickNumber = 12;
-    unemploymentLine.drawChart();
-    unemploymentLine.addTooltip("Unemployment Annual % Change - ", "percentage2", "label");
-    unemploymentLine.hideRate(true);
+  unemploymentLine = new MultiLineChart(unempContent)
+  unemploymentLine.tickNumber = 12
+  unemploymentLine.drawChart()
+  unemploymentLine.addTooltip('Unemployment Annual % Change - ', 'percentage2', 'label')
+  unemploymentLine.hideRate(true)
 
-    d3.select("#chart-emp-rate").style("display", "none");
+  d3.select('#chart-emp-rate').style('display', 'none')
 
-    d3.select(".employment_count").on("click", function() {
-      activeBtn(this);
-      d3.select("#chart-employment").style("display", "block");
-      d3.select("#chart-emp-rate").style("display", "none");
-      //need to redraw in case window size has changed
-      employmentStack.tickNumber = 12;
-      employmentStack.drawChart();
-      employmentStack.addTooltip("Thousands - Quarter:", "thousands", "label");
-
-    });
-
-    d3.select(".employment_arate").on("click", function() {
-      activeBtn(this);
-      d3.select("#chart-employment").style("display", "none");
-      d3.select("#chart-emp-rate").style("display", "block");
-      employmentLine.tickNumber = 10;
-      employmentLine.drawChart();
-      employmentLine.addTooltip("Employment Annual % Change - ", "percentage2", "label");
-      employmentLine.hideRate(true);
-    });
-
-    d3.select("#chart-unemp-rate").style("display", "none");
-
-    d3.select(".unemployment_count").on("click", function() {
-      activeBtn(this);
-      d3.select("#chart-unemployment").style("display", "block");
-      d3.select("#chart-unemp-rate").style("display", "none");
-      unemploymentStack.tickNumber = 12;
-      unemploymentStack.drawChart();
-      unemploymentStack.addTooltip("Thousands - Quarter:", "thousands", "label");
-    });
-
-    d3.select(".unemployment_arate").on("click", function() {
-      activeBtn(this);
-      d3.select("#chart-unemployment").style("display", "none");
-      d3.select("#chart-unemp-rate").style("display", "block");
-      unemploymentLine.tickNumber = 10;
-      unemploymentLine.drawChart();
-      unemploymentLine.addTooltip("Unemployment Annual % Change - ", "percentage2", "label");
-      unemploymentLine.hideRate(true);
-    });
-
+  d3.select('.employment_count').on('click', function () {
+    activeBtn(this)
+    d3.select('#chart-employment').style('display', 'block')
+    d3.select('#chart-emp-rate').style('display', 'none')
+      // need to redraw in case window size has changed
+    employmentStack.tickNumber = 12
+    employmentStack.drawChart()
+    employmentStack.addTooltip('Thousands - Quarter:', 'thousands', 'label')
   })
-  .catch(function(error) {
-    console.log(error);
-  });
 
-/*** This the Gross Value Added per Capita at Basic Prices Chart ***/
+  d3.select('.employment_arate').on('click', function () {
+    activeBtn(this)
+    d3.select('#chart-employment').style('display', 'none')
+    d3.select('#chart-emp-rate').style('display', 'block')
+    employmentLine.tickNumber = 10
+    employmentLine.drawChart()
+    employmentLine.addTooltip('Employment Annual % Change - ', 'percentage2', 'label')
+    employmentLine.hideRate(true)
+  })
+
+  d3.select('#chart-unemp-rate').style('display', 'none')
+
+  d3.select('.unemployment_count').on('click', function () {
+    activeBtn(this)
+    d3.select('#chart-unemployment').style('display', 'block')
+    d3.select('#chart-unemp-rate').style('display', 'none')
+    unemploymentStack.tickNumber = 12
+    unemploymentStack.drawChart()
+    unemploymentStack.addTooltip('Thousands - Quarter:', 'thousands', 'label')
+  })
+
+  d3.select('.unemployment_arate').on('click', function () {
+    activeBtn(this)
+    d3.select('#chart-unemployment').style('display', 'none')
+    d3.select('#chart-unemp-rate').style('display', 'block')
+    unemploymentLine.tickNumber = 10
+    unemploymentLine.drawChart()
+    unemploymentLine.addTooltip('Unemployment Annual % Change - ', 'percentage2', 'label')
+    unemploymentLine.hideRate(true)
+  })
+})
+  .catch(function (error) {
+    console.log(error)
+  })
+
+/** * This the Gross Value Added per Capita at Basic Prices Chart ***/
 // d3.csv("../data/Economy/RAA01.csv").then( data => {
 //
 //     let columnNames = data.columns.slice(1),
@@ -397,112 +388,66 @@ Promise.all([
 //     console.log(error);
 //     });
 
-
-function join(lookupTable, mainTable, lookupKey, mainKey, select) {
-
+function join (lookupTable, mainTable, lookupKey, mainKey, select) {
   var l = lookupTable.length,
     m = mainTable.length,
     lookupIndex = [],
-    output = [];
+    output = []
 
   for (var i = 0; i < l; i++) { // loop through the lookup array
-    var row = lookupTable[i];
-    lookupIndex[row[lookupKey]] = row; // create a index for lookup table
+    var row = lookupTable[i]
+    lookupIndex[row[lookupKey]] = row // create a index for lookup table
   }
 
   for (var j = 0; j < m; j++) { // loop through m items
-    var y = mainTable[j];
-    var x = lookupIndex[y[mainKey]]; // get corresponding row from lookupTable
-    output.push(select(y, x)); // select only the columns you need
+    var y = mainTable[j]
+    var x = lookupIndex[y[mainKey]] // get corresponding row from lookupTable
+    output.push(select(y, x)) // select only the columns you need
   }
 
-  return output;
+  return output
 }
 
-function coerceNum(d, k) {
-  d.forEach(d => {
-    for (var i = 0, n = k.length; i < n; i++) {
-      d[k[i]] = d[k[i]] !== "null" ? +d[k[i]] : "unavailable";
-    }
-    return d;
-  });
+function d3Nest (d, n) {
+  let nest = d3.nest()
+    .key(name => {
+      return name[n]
+    })
+    .entries(d)
+  return nest
 }
 
-function stackNest(data, date, name, value) {
+function filterByDateRange (data, dateField, dateOne, dateTwo) {
+  return data.filter(d => {
+    return d[dateField] >= new Date(dateOne) && d[dateField] <= new Date(dateTwo)
+  })
+}
+
+function filterbyDate (data, dateField, date) {
+  return data.filter(d => {
+    return d[dateField] >= new Date(date)
+  })
+}
+
+function stackNest (data, date, name, value) {
   let nested_data = d3Nest(data, date),
-    mqpdata = nested_data.map(function(d) {
+    mqpdata = nested_data.map(function (d) {
       let obj = {
         label: d.key
       }
-      d.values.forEach(function(v) {
-        obj.date = v.date;
-        obj.year = v.year;
-        obj[v[name]] = v[value];
+      d.values.forEach(function (v) {
+        obj.date = v.date
+        obj.year = v.year
+        obj[v[name]] = v[value]
       })
-      return obj;
+      return obj
     })
-  return mqpdata;
+  return mqpdata
 }
 
-function d3Nest(d, n) {
-  let nest = d3.nest()
-    .key(name => {
-      return name[n];
-    })
-    .entries(d);
-  return nest;
-}
-
-function parseYearDates(d, v) {
-  d.forEach(d => {
-    d.label = d[v];
-    d[v] = parseYear(d[v]);
-  });
-}
-
-function parseQuarter(d, v) {
-  d.forEach(d => {
-    d.label = d[v];
-    d.date = convertQuarter(d[v]);
-    d[v] = qToQuarter(d[v]);
-    d.year = formatYear(d.date);
-  });
-}
-
-function convertQuarter(q) {
-  let splitted = q.split('Q'),
-    year = splitted[0],
-    quarterEndMonth = splitted[1] * 3 - 2,
-    date = d3.timeParse('%m %Y')(quarterEndMonth + ' ' + year);
-
-  return date;
-}
-
-function qToQuarter(q) {
-  let splitted = q.split('Q'),
-    year = splitted[0],
-    quarter = splitted[1],
-    quarterString = (year + " Quarter " + quarter);
-
-  return quarterString;
-}
-
-
-function filterByDateRange(data, dateField, dateOne, dateTwo) {
-  return data.filter(d => {
-    return d[dateField] >= new Date(dateOne) && d[dateField] <= new Date(dateTwo);
-  });
-}
-
-function filterbyDate(data, dateField, date) {
-  return data.filter(d => {
-    return d[dateField] >= new Date(date);
-  });
-}
-
-function activeBtn(e) {
-  let btn = e;
-  $(btn).siblings().removeClass('active');
-  $(btn).addClass('active');
+function activeBtn (e) {
+  let btn = e
+  $(btn).siblings().removeClass('active')
+  $(btn).addClass('active')
 }
 // d3.selectAll(".chart-holder_PH").attr("class", "chart-holder");
--- public/javascripts/themes/themes_common.js
+++ public/javascripts/themes/themes_common.js
@@ -1,112 +1,105 @@
-d3.select(window).on("resize", function() {
-  let w = window.innerWidth;
-  console.log("Resize: " + w);
-  dublinBikesChart.drawChart();
-  dublinBikesChart.addTooltip("Dublin Bikes at ", "thousands", "label", "", "");
+d3.select(window).on('resize', function () {
+  let w = window.innerWidth
+  console.log('Resize: ' + w)
+  dublinBikesChart.drawChart()
+  dublinBikesChart.addTooltip('Dublin Bikes at ', 'thousands', 'label', '', '')
 
-  supplyChart.drawChart();
-  supplyChart.addTooltip("Land - Year", "thousands", "label");
+  supplyChart.drawChart()
+  supplyChart.addTooltip('Land - Year', 'thousands', 'label')
 
-  houseCompCharts.drawChart();
-  houseCompCharts.addTooltip("Units by Month:", "thousands", "year");
+  houseCompCharts.drawChart()
+  houseCompCharts.addTooltip('Units by Month:', 'thousands', 'year')
 
-  hCBTChart.drawChart();
-  hCBTChart.addTooltip("Total Houses - ", "thousands", "label");
+  hCBTChart.drawChart()
+  hCBTChart.addTooltip('Total Houses - ', 'thousands', 'label')
 
-  newCompByTypeChart.drawChart();
-  newCompByTypeChart.addTooltip("Total Houses - ", "thousands", "label");
+  newCompByTypeChart.drawChart()
+  newCompByTypeChart.addTooltip('Total Houses - ', 'thousands', 'label')
 
-  contributionChart.drawChart();
-  contributionChart.addTooltip("In Millions - Year ", "millions", "label", "€");
+  contributionChart.drawChart()
+  contributionChart.addTooltip('In Millions - Year ', 'millions', 'label', '€')
 
-  housePricesChart.drawChart();
-  housePricesChart.addTooltip("In thousands - ", "thousands", "label", "€");
+  housePricesChart.drawChart()
+  housePricesChart.addTooltip('In thousands - ', 'thousands', 'label', '€')
 
-  rentPricesChart.drawChart();
-  rentPricesChart.addTooltip("In thousands - ", "thousands", "label", "€");
+  rentPricesChart.drawChart()
+  rentPricesChart.addTooltip('In thousands - ', 'thousands', 'label', '€')
 
-  rentByBedsChart.drawChart();
-  rentByBedsChart.addTooltip(rentByBedTT);
+  rentByBedsChart.drawChart()
+  rentByBedsChart.addTooltip(rentByBedTT)
 
-  dccChart.drawChart();
-  drccChart.drawChart();
-  fccChart.drawChart();
-  sdccChart.drawChart();
+  dccChart.drawChart()
+  drccChart.drawChart()
+  fccChart.drawChart()
+  sdccChart.drawChart()
 
-  dccChart.addTooltip(planningTT);
-  drccChart.addTooltip(planningTT);
-  fccChart.addTooltip(planningTT);
-  sdccChart.addTooltip(planningTT);
+  dccChart.addTooltip(planningTT)
+  drccChart.addTooltip(planningTT)
+  fccChart.addTooltip(planningTT)
+  sdccChart.addTooltip(planningTT)
 
+  HPM06Charts.drawChart() // draw axis
+  HPM06Charts.addTooltip('Price Index - ', '', 'label') // add tooltip
+  HPM06Charts.addBaseLine(100) // add horizontal baseline
 
-  HPM06Charts.drawChart(); // draw axis
-  HPM06Charts.addTooltip("Price Index - ", "", "label"); // add tooltip
-  HPM06Charts.addBaseLine(100); // add horizontal baseline
+  wasteChart.drawChart()
+  wasteChart.addTooltip('Waste - Year ', 'thousands', 'label', '', 'Kg')
+  recyclingsChart.drawChart()
+  recyclingsChart.addTooltip(recyclings_tooltip)
+  organicrecyclingsChart.drawChart()
+  organicrecyclingsChart.addTooltip(orChart_tooltip)
+  waterconsChart.drawChart()
+  waterconsChart.addTooltip(wcChart_tooltip)
+  riverqualitiesChart.drawChart()
+  riverqualitiesChart.addTooltip(rqChart_tooltip)
+  greenflagsChart.drawChart()
+  greenflagsChart.addTooltip(greenflags_tooltip)
+  localagendasChart.drawChart()
+  localagendasChart.addTooltip('Projects - Year ', 'thousands', 'label', '', '')
 
-
-
-  wasteChart.drawChart();
-  wasteChart.addTooltip("Waste - Year ", "thousands", "label", "", "Kg");
-  recyclingsChart.drawChart();
-  recyclingsChart.addTooltip(recyclings_tooltip);
-  organicrecyclingsChart.drawChart();
-  organicrecyclingsChart.addTooltip(orChart_tooltip);
-  waterconsChart.drawChart();
-  waterconsChart.addTooltip(wcChart_tooltip);
-  riverqualitiesChart.drawChart();
-  riverqualitiesChart.addTooltip(rqChart_tooltip);
-  greenflagsChart.drawChart();
-  greenflagsChart.addTooltip(greenflags_tooltip);
-  localagendasChart.drawChart();
-  localagendasChart.addTooltip("Projects - Year ", "thousands", "label", "", "");
-
-  //if width is smaller select fewer labels for chart
+  // if width is smaller select fewer labels for chart
   // let populationChartLabels = [0, 16, 26, 36, 46, 56, 69, 76, 81, 86, 92, 96, 101, 106]: [0, 26, 41, 51, 61, 71, 81, 92, 101, 106];
-  populationChart.tickNumber = 106;
-  populationChart.drawChart();
-  populationChart.addTooltip("Year: ", "thousands", "label");
-  populationChart.showSelectedLabels([0, 16, 26, 36, 46, 56, 66, 76, 86, 96, 106]);
+  populationChart.tickNumber = 106
+  populationChart.drawChart()
+  populationChart.addTooltip('Year: ', 'thousands', 'label')
+  populationChart.showSelectedLabels([0, 16, 26, 36, 46, 56, 66, 76, 86, 96, 106])
   // populationChart.showSelectedLabels([0, 16, 26, 36, 41, 46, 51, 56, 61, 69, 71, 76, 81, 86, 92, 96, 101, 106]);
 
-  outsideStateChart.drawChart();
-  outsideStateChart.addTooltip(outsideStateTT);
-  houseHoldsChart.drawChart();
-  houseHoldsChart.addTooltip(houseHoldsTT);
-  houseHoldCompositionChart.drawChart();
-  houseHoldCompositionChart.addTooltip(houseHoldCompositionTT);
+  outsideStateChart.drawChart()
+  outsideStateChart.addTooltip(outsideStateTT)
+  houseHoldsChart.drawChart()
+  houseHoldsChart.addTooltip(houseHoldsTT)
+  houseHoldCompositionChart.drawChart()
+  houseHoldCompositionChart.addTooltip(houseHoldCompositionTT)
 
   // Employment charts use 2 charts, 1 hidden by display; need to check which is active
-  if (d3.select(".employment_count").classed('active')) {
-    d3.select("#chart-employment").style("display", "block");
-    d3.select("#chart-emp-rate").style("display", "none");
-    employmentStack.drawChart();
-    employmentStack.addTooltip("Thousands - Quarter:", "thousands", "label");
-    //console.log("#chart-employment is active");
-
+  if (d3.select('.employment_count').classed('active')) {
+    d3.select('#chart-employment').style('display', 'block')
+    d3.select('#chart-emp-rate').style('display', 'none')
+    employmentStack.drawChart()
+    employmentStack.addTooltip('Thousands - Quarter:', 'thousands', 'label')
+    // console.log("#chart-employment is active");
   } else {
-    d3.select("#chart-employment").style("display", "none");
-    d3.select("#chart-emp-rate").style("display", "block");
-    employmentLine.drawChart();
-    employmentLine.addTooltip("Employment Annual % Change - ", "percentage2", "label");
-    employmentLine.hideRate(true);
-    //console.log("#chart-employment is not active")
+    d3.select('#chart-employment').style('display', 'none')
+    d3.select('#chart-emp-rate').style('display', 'block')
+    employmentLine.drawChart()
+    employmentLine.addTooltip('Employment Annual % Change - ', 'percentage2', 'label')
+    employmentLine.hideRate(true)
+    // console.log("#chart-employment is not active")
   };
 
-  if (d3.select(".unemployment_count").classed('active')) {
-    d3.select("#chart-unemployment").style("display", "block");
-    d3.select("#chart-unemp-rate").style("display", "none");
-    unemploymentStack.drawChart();
-    unemploymentStack.addTooltip("Thousands - Quarter:", "thousands", "label");
-    //console.log("#chart-employment is active");
-
+  if (d3.select('.unemployment_count').classed('active')) {
+    d3.select('#chart-unemployment').style('display', 'block')
+    d3.select('#chart-unemp-rate').style('display', 'none')
+    unemploymentStack.drawChart()
+    unemploymentStack.addTooltip('Thousands - Quarter:', 'thousands', 'label')
+    // console.log("#chart-employment is active");
   } else {
-    d3.select("#chart-unemployment").style("display", "none");
-    d3.select("#chart-unemp-rate").style("display", "block");
-    unemploymentLine.drawChart();
-    unemploymentLine.addTooltip("Unemployment Annual % Change - ", "percentage2", "label");
-    unemploymentLine.hideRate(true);
+    d3.select('#chart-unemployment').style('display', 'none')
+    d3.select('#chart-unemp-rate').style('display', 'block')
+    unemploymentLine.drawChart()
+    unemploymentLine.addTooltip('Unemployment Annual % Change - ', 'percentage2', 'label')
+    unemploymentLine.hideRate(true)
     // console.log("unemployment_count is not active")
   };
-
-
-});
+})
