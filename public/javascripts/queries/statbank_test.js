9889f896a29c6beb2b170645f2fcca88909d5638
LiamOSullivan <lmosulli@tcd.ie>
2020-01-31 16:07:17 +0000

    Basic statbank datalist

--- public/javascripts/queries/statbank_test.js
+++ public/javascripts/queries/statbank_test.js
@@ -6,12 +6,12 @@ import { datalist } from 'https://unpkg.com/jsonstat-utils@2.5.5/export.mjs'
 let main = async function (url) {
   const res = await fetch(url)
   const json = await res.json()
-  document.getElementById('statbank-test-chart').innerHTML = datalist(json, {
-    counter: true,
-    tblclass: 'datalist',
-    numclass: 'number',
-    valclass: 'value',
-    vlabel: 'value'
+  document.getElementById('statbank-test-datalist').innerHTML = datalist(json, {
+    counter: false,
+    tblclass: 'datalist'
+    // numclass: 'number'
+    // valclass: 'value'
+    // vlabel: 'value'
   })
 }
 main(url_peb06)
