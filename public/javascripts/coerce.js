9e6d4b01c221aeb7592a1fb15a4df0417a4bb27d
LiamOSullivan <lmosulli@tcd.ie>
2020-02-28 08:28:12 +0000

    Minor changes to  save

--- public/javascripts/coerce.js
+++ public/javascripts/coerce.js
@@ -45,4 +45,3 @@ let locale = d3.formatLocale({
   'months': ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
   'shortMonths': ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 })
-
