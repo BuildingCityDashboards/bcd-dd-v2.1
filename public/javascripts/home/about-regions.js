b424d85e0f0a365fa8a23af758c9fb000321d765
LiamOSullivan <lmosulli@tcd.ie>
2020-03-10 16:05:08 +0000

    Load Dublin map svg, style and add interaction

---
+++ public/javascripts/home/about-regions.js
@@ -0,0 +1,41 @@
+d3.xml('/images/home/dublin-regions-map.svg')
+  .then(xml => {
+    // "xml" is the XML DOM tree
+    let htmlSVG = document.getElementById('map') // the svg-element in our HTML file
+    // append the "maproot" group to the svg-element in our HTML file
+    htmlSVG.appendChild(xml.documentElement.getElementById('Dublin-ZoneMap'))
+
+    // d3 objects for later use
+    let dublinSvg = d3.select(htmlSVG)
+    let dccRoot = dublinSvg.select('#Dublin_City')
+    let dlrRoot = dublinSvg.select('#Dun_Laoghaire_Rathdown')
+    let sdRoot = dublinSvg.select('#South_Dublin')
+    let fRoot = dublinSvg.select('#Fingal')
+
+    // get the svg-element from the original SVG file
+    const xmlSVG = d3.select(xml.getElementsByTagName('svg')[0])
+    // copy its "viewBox" attribute to the svg element in our HTML file
+    dublinSvg.attr('viewBox', xmlSVG.attr('viewBox'))
+
+    let dccPath = dccRoot.select('path')
+    let dlrPath = dlrRoot.select('path')
+    let sdPath = sdRoot.select('path')
+    let fPath = fRoot.select('path')
+
+    let paths = [dccPath, dlrPath, sdPath, fPath]
+    paths.forEach(p => {
+      p.on('mouseover', function () {
+        d3.select(this).style('fill', 'red')
+      })
+      p.on('mouseout', function () {
+        d3.select(this).style('fill', d3.select(this).style('fillDefault'))
+      })
+      p.on('click', function () {
+        d3.select(this).style('fill', 'green')
+      })
+    })
+  })
+  .catch(e => {
+    console.log('error' + e)
+  })
+
