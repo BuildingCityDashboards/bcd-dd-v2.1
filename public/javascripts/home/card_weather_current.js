c0faeb9e31829bbeafde8223bb2d9d182d4b4a9f
LiamOSullivan <lmosulli@tcd.ie>
2020-04-07 19:23:47 +0100

    Populate weather card with basic data

--- public/javascripts/home/card_weather_current.js
+++ public/javascripts/home/card_weather_current.js
@@ -3,9 +3,9 @@
 const fetchWeatherDataLatest = function () {
   d3.xml('/api/weather/latest')
     .then((data) => {
-      console.log('Fetched Weather card data ')
-      console.log(data)
-      // processWeather(data)
+      console.log('Fetched Weather card data - latest observations')
+      // console.log(data)
+      processWeather(data)
     })
     .catch(function (err) {
       console.error('Error fetching Weather card data: ' + JSON.stringify(err))
@@ -24,81 +24,131 @@ fetchWeatherDataLatest()
 // )
 
 function processWeather (xmlWeather) {
-  let modelXML = xmlWeather.getElementsByTagName('model')
-  let forecastTime = moment(modelXML[0].getAttribute('runended')).fromNow()
-
-  // console.log('forecastTime: ' + JSON.stringify(forecastTime))
-
-  let timesXML = xmlWeather.getElementsByTagName('time')
-  // console.log('#timesXML: ' + timesXML.length)
-  // use index in loop to track odd/even entry
-  let forecasts = [] // array of objects
-  let id = 0
-  /* TODO: fix hack */
-  for (let i = 0; i < 4; i += 2) {
-    let from, startDate, locationEven, lat, lng, temp, humidity, windDir, windSpeed,
-      locationOdd, symbolId, symbolNo, precip
-    // console.log('TIME #' + i)
-    from = timesXML[i].getAttribute('from')
-    startDate = new Date(from)
-    // console.log('from: ' + from)
-    locationEven = timesXML[i].getElementsByTagName('location')[0]
-    lat = locationEven.getAttribute('latitude')
-    // console.log("lat: " + lat);
-    lng = locationEven.getAttribute('longitude')
-    // console.log("lng: " + lng);
-    temp = locationEven.getElementsByTagName('temperature')[0].getAttribute('value')
-    // console.log("temp : " + temp);
-    humidity = locationEven.getElementsByTagName('humidity')[0].getAttribute('value')
-    // console.log("humidity : " + humidity);
-    windDir = locationEven.getElementsByTagName('windDirection')[0].getAttribute('name')
-    // console.log("windDir : " + windDir);
-    windSpeed = locationEven.getElementsByTagName('windSpeed')[0].getAttribute('beaufort')
-    // console.log("i : " + i);
-    // console.log('windspeed : ' + windSpeed)
-    // Odd entries
-    locationOdd = timesXML[i + 1].getElementsByTagName('location')[0]
-    // console.log("location odd: " + JSON.stringify(locationOdd.getElementsByTagName("symbol")[0]));
-    symbolId = locationOdd.getElementsByTagName('symbol')[0].getAttribute('id')
-    // console.log("symbol ID: " + symbolId);
-    symbolNo = locationOdd.getElementsByTagName('symbol')[0].getAttribute('number')
-    // console.log("symbol #" + symbolNo);
-    symbolNo = +symbolNo
-    if (symbolNo < 10) {
-      symbolNo = '0' + symbolNo // pad with a zero
+  let observations = xmlWeather.getElementsByTagName('observations')
+  let observationsTime = observations[0].getAttribute('time')
+  console.log('weather for: ' + JSON.stringify(observationsTime))
+  // let arr = observations[0].getElementsByTagName('station').namedItem('Dublin')
+  let stations = observations[0].getElementsByTagName('station')
+  for (s of stations) {
+    if (s.getAttribute('name') === 'Dublin') {
+      console.log()
+      console.log(getStringForAttribute(s, 'weather_text'))
+      console.log()
+      console.log()
+      console.log(getStringForAttribute(s, 'humidity'))
+      console.log()
+      console.log(getStringForAttribute(s, 'pressure'))
     }
-    // console.log('date: ' + startDate + ' start hour: ' + startDate.getHours())
-    // decide if night or day based on hour
-    // /*TODO: Crude! Improve!*/
-    if (startDate.getHours() > 18 || startDate.getHours() < 6) {
-      tod = 'n'
-    } else {
-      tod = 'd'
-    }
-    precip = locationOdd.getElementsByTagName('precipitation')[0].getAttribute('value')
-    // console.log("precip #" + precip);
-    //
-    // Only use the next 48 hourly readings
-    /* TODO: Better algo to decide if readings are to be used/ are valid */
-    if (id < 48) {
-      forecasts.push({
-        'id': id,
-        'date': startDate,
-        'temperature': temp,
-        'humidity': humidity,
-        'windDir': windDir,
-        'windSpeed': windSpeed,
-        'symbolNo': symbolNo,
-        'symbolId': symbolId,
-        'precip': precip,
-        'tod': tod
-
-      })
-    }
-    id += 1
   }
-  updateWeatherDisplay(forecasts, forecastTime)
-};
+
+  d3.select('#hero-weather__left-top')
+      .html(getStringForAttribute(s, 'temp'))
+  // //
+  d3.select('#hero-weather__left-bottom')
+        .html('Prec ' + getStringForAttribute(s, 'rainfall'))
+
+  d3.select('#hero-weather__right-top')
+        .html('<img src = "/images/Met50v2/15d.png">' + '  ' + getStringForAttribute(s, 'wind_direction'))
+
+  d3.select('#hero-weather__right-bottom')
+        .html(getStringForAttribute(s, 'wind_speed'))
+
+  // d3.select('#hero-weather__symbol')
+  //     .html('<img src = "/images/Met50v2/' + f[0].symbolNo + f[0].tod + '.png">')
+
+  // updateWeatherDisplay(forecasts, forecastTime)
+}
+
+function getStringForAttribute (e, n) {
+  e = e.getElementsByTagName(n)[0]
+  let u = ''
+  if (e.hasAttribute('unit')) {
+    u = ' ' + e.getAttribute('unit')
+  }
+  let v = e.innerHTML.trim()
+  return v + u
+}
+
+//   let unit = ''
+
+// if (t.hasAttribute('unit')) {
+//   unit = t.getAttribute('unit')
+// }
+// res[] = t.innerHTML + ' ' + unit
+
+  // let test = ['something', 'something', 'something']
+  // arrTest = test.map((s) => {
+  //   return 'Dublin'
+  // })
+
+  // let timesXML = xmlWeather.getElementsByTagName('time')
+  // // console.log('#timesXML: ' + timesXML.length)
+  // // use index in loop to track odd/even entry
+  // let forecasts = [] // array of objects
+  // let id = 0
+  // /* TODO: fix hack */
+  // for (let i = 0; i < 4; i += 2) {
+  //   let from, startDate, locationEven, lat, lng, temp, humidity, windDir, windSpeed,
+  //     locationOdd, symbolId, symbolNo, precip
+  //   // console.log('TIME #' + i)
+  //   from = timesXML[i].getAttribute('from')
+  //   startDate = new Date(from)
+  //   // console.log('from: ' + from)
+  //   locationEven = timesXML[i].getElementsByTagName('location')[0]
+  //   lat = locationEven.getAttribute('latitude')
+  //   // console.log("lat: " + lat);
+  //   lng = locationEven.getAttribute('longitude')
+  //   // console.log("lng: " + lng);
+  //   temp = locationEven.getElementsByTagName('temperature')[0].getAttribute('value')
+  //   // console.log("temp : " + temp);
+  //   humidity = locationEven.getElementsByTagName('humidity')[0].getAttribute('value')
+  //   // console.log("humidity : " + humidity);
+  //   windDir = locationEven.getElementsByTagName('windDirection')[0].getAttribute('name')
+  //   // console.log("windDir : " + windDir);
+  //   windSpeed = locationEven.getElementsByTagName('windSpeed')[0].getAttribute('beaufort')
+  //   // console.log("i : " + i);
+  //   // console.log('windspeed : ' + windSpeed)
+  //   // Odd entries
+  //   locationOdd = timesXML[i + 1].getElementsByTagName('location')[0]
+  //   // console.log("location odd: " + JSON.stringify(locationOdd.getElementsByTagName("symbol")[0]));
+  //   symbolId = locationOdd.getElementsByTagName('symbol')[0].getAttribute('id')
+  //   // console.log("symbol ID: " + symbolId);
+  //   symbolNo = locationOdd.getElementsByTagName('symbol')[0].getAttribute('number')
+  //   // console.log("symbol #" + symbolNo);
+  //   symbolNo = +symbolNo
+  //   if (symbolNo < 10) {
+  //     symbolNo = '0' + symbolNo // pad with a zero
+  //   }
+  //   // console.log('date: ' + startDate + ' start hour: ' + startDate.getHours())
+  //   // decide if night or day based on hour
+  //   // /*TODO: Crude! Improve!*/
+  //   if (startDate.getHours() > 18 || startDate.getHours() < 6) {
+  //     tod = 'n'
+  //   } else {
+  //     tod = 'd'
+  //   }
+  //   precip = locationOdd.getElementsByTagName('precipitation')[0].getAttribute('value')
+  //   // console.log("precip #" + precip);
+  //   //
+  //   // Only use the next 48 hourly readings
+  //   /* TODO: Better algo to decide if readings are to be used/ are valid */
+  //   if (id < 48) {
+  //     forecasts.push({
+  //       'id': id,
+  //       'date': startDate,
+  //       'temperature': temp,
+  //       'humidity': humidity,
+  //       'windDir': windDir,
+  //       'windSpeed': windSpeed,
+  //       'symbolNo': symbolNo,
+  //       'symbolId': symbolId,
+  //       'precip': precip,
+  //       'tod': tod
+  //
+  //     })
+  //   }
+  //   id += 1
+  // }
 
 function initialiseWeatherDisplay () {
   // d3.select('#weather-card').select('.card__header')
