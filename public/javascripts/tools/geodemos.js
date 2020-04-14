7e3ad817e5147b7d2f4714f250df6eefcabbf6b1
LiamOSullivan <lmosulli@tcd.ie>
2020-04-14 15:56:52 +0100

    Reflow logic

--- public/javascripts/tools/geodemos.js
+++ public/javascripts/tools/geodemos.js
@@ -1,3 +1,12 @@
+/**  TODOs
+Load SAs
+Add cluster number to SA objects Needs to happen after SAs loaded
+Filter based on cluster/group number
+Add zscores data
+Display zscores widget and filter based on group selection
+Use zscores widget to filter map
+**/
+
 let dub_lng = -6.2603
 let dub_lat = 53.42
 let dublinX, dublinY
@@ -15,66 +24,52 @@ let osmAttrib = 'Map data © <a href="http://openstreetmapGeodemos.org">OpenStre
 let osmAttrib_Hot = '&copy; <a href="http://www.openstreetmapGeodemos.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetmapGeodemos.org/" target="_blank">Humanitarian OpenStreetMap Team</a>'
 let stamenTonerAttrib = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmapGeodemos.org/copyright">OpenStreetMap</a>'
 let mapGeodemos = new L.Map('map-geodemos')
-let osm = new L.TileLayer(stamenTonerUrl_Lite, {
+let osm = new L.TileLayer(osmUrl_BW, {
   minZoom: min_zoom,
   maxZoom: max_zoom,
-  attribution: stamenTonerAttrib
+  attribution: osmAttrib
 })
 mapGeodemos.setView(new L.LatLng(dub_lat, dub_lng), zoom)
 mapGeodemos.addLayer(osm)
-// crossfilter variables
-// let idDim;
-// d3.csv("/data/tools/census2016/SAPS2016_SA2017.csv").then(function (data) {
-// //    processVariables(data);
-// });
-let idDim // data dimension accessible by GEOGID
-function processVariables (data_) {
-  //    data_.forEach(function (d) {
-  //        d.id = +d.GEOGID.split('_')[1]; //extract the numerical part
-  //        //corresponding to the boundary geojson
-  //    });
-  console.log('Variables length = ' + data_.length)
-  let censusDataXF = crossfilter(data_)
-  idDim = censusDataXF.dimension(function (d) {
-    return +d.GEOGID.split('_')[1]
-  })
-}
 
+loadSmallAreas()
+function loadSmallAreas () {
 // Incrementally load boundaries for each LA
-let dataBase = '/data/tools/census2016/'
-let dcc0 = 'DCC_SA_0.geojson'
-let dcc1 = 'DCC_SA_1.geojson'
-let dcc2 = 'DCC_SA_2.geojson'
+  let dataBase = '/data/tools/census2016/'
+  let dcc0 = 'DCC_SA_0.geojson'
+  let dcc1 = 'DCC_SA_1.geojson'
+  let dcc2 = 'DCC_SA_2.geojson'
 // promises
-let pDCC0 = d3.json(dataBase + dcc0)
-let pDCC1 = d3.json(dataBase + dcc1)
-let pDCC2 = d3.json(dataBase + dcc2)
+  let pDCC0 = d3.json(dataBase + dcc0)
+  let pDCC1 = d3.json(dataBase + dcc1)
+  let pDCC2 = d3.json(dataBase + dcc2)
 // DCC
-Promise.all([pDCC0, pDCC1, pDCC2])
+  Promise.all([pDCC0, pDCC1, pDCC2])
   .then(function (dArr) {
     updateMap(join(dArr))
   })
 // Fingal
-let fcc0 = 'FCC_SA_0.geojson'
-let pFCC0 = d3.json(dataBase + fcc0)
-Promise.all([pFCC0])
+  let fcc0 = 'FCC_SA_0.geojson'
+  let pFCC0 = d3.json(dataBase + fcc0)
+  Promise.all([pFCC0])
   .then(function (dArr) {
     updateMap(join(dArr))
   })
 // DL/R
-let dlr0 = 'DLR_SA_0.geojson'
-let pDLR0 = d3.json(dataBase + dlr0)
-Promise.all([pDLR0])
+  let dlr0 = 'DLR_SA_0.geojson'
+  let pDLR0 = d3.json(dataBase + dlr0)
+  Promise.all([pDLR0])
   .then(function (dArr) {
     updateMap(join(dArr))
   })
 // SDCC
-let sdcc0 = 'SDCC_SA_0.geojson'
-let pSDCC0 = d3.json(dataBase + sdcc0)
-Promise.all([pSDCC0])
+  let sdcc0 = 'SDCC_SA_0.geojson'
+  let pSDCC0 = d3.json(dataBase + sdcc0)
+  Promise.all([pSDCC0])
   .then(function (dArr) {
     updateMap(join(dArr))
   })
+}
 
 function join (dArr_) {
   //    console.log('Boundaries length = ' + dArr_.length);
@@ -103,22 +98,37 @@ function updateMap (data__) {
     onEachFeature: onEachFeature
   })
 
-  function onEachFeature (feature, layer) {
-    layer.bindPopup(
+  boundaries.addTo(mapGeodemos)
+}
+
+function style (f) {
+  // console.log("style feature "+f.properties.COUNTYNAME)
+  return {
+    fillColor: getCountyColor(f.properties.COUNTYNAME),
+    weight: 1,
+    opacity: 2,
+    color: getCountyColor(f.properties.COUNTYNAME),
+    dashArray: '1',
+    fillOpacity: 0.5
+  }
+};
+
+function onEachFeature (feature, layer) {
+  layer.bindPopup(
                 '<p><b>' + feature.properties.EDNAME + '</b></p>' +
                 '<p>' + feature.properties.COUNTYNAME + '</p>' +
                 '<p>SA ' + feature.properties.SMALL_AREA + '</p>'
                 )
         // bind click
-    layer.on({
-      click: function () {
+  layer.on({
+    click: function () {
                 // idDim.filter(feature.properties.EDNAME);
                 // let res = idDim.top(Infinity)[0].T1_1AGE1;
                 //                console.log(idDim.top(Infinity));
 
-        d3.select('#data-title')
+      d3.select('#data-title')
           .html(feature.properties.EDNAME)
-        d3.select('#data-subtitle')
+      d3.select('#data-subtitle')
           .html(feature.properties.COUNTYNAME + ', Small Area ' + feature.properties.SMALL_AREA)
         // d3.select("#data-display")
         //   .html(JSON.stringify(feature.properties));
@@ -132,103 +142,121 @@ function updateMap (data__) {
         //   d3.select('#data-chart-title')
         //     .html('Age distribution of males')
         // })
-      }
-    })
-  }
-
-  function formatData (data_) {
-    // let res = JSON.stringify(data_, null, '\n');
-
-    let keys = d3.keys(data_[0])
-    // Mens ages from index 4 to 37 inclusive
-    // 18+ from index 22
-    let over18Males = 0
-    for (let i = 22; i < 38; i += 1) {
-      over18Males += data_[0][keys[i]]
-      // console.log(keys[i]+" : "+data_[0][keys[i]]);
     }
-    let over18Females = 0
-    // Female ages from index 39 to 72 inclusive
-    // 18+ from index 57
-    for (let i = 57; i < 73; i += 1) {
-      over18Females += data_[0][keys[i]]
-      // console.log(keys[i]+" : "+data_[0][keys[i]]);
-    }
-    let totalMale = data_[0]['T1_1AGETM']
-    let totalFemale = data_[0]['T1_1AGETF']
-    let textOutput1 = 'The population of this small area is <b>' +
-      totalMale + ' males</b> and <b>' +
-      totalFemale + ' females</b>, giving <b>' +
-      (totalMale + totalFemale) + '</b> persons in total. '
-    let textOutput2 = 'Of those, <b>' +
-      over18Males + ' men</b> and <b>' +
-      over18Females + ' women</b> are over 18 years of age. <br>'
-    return textOutput1 + textOutput2
-  }
+  })
+}
 
-  let chartMargins = [10, 0, 30, 20]
-  // (map size - margins)/2 map size is specified in the css for leaflet charts
-  let chartHeight = 250 // (200 - chartMargins[0] - chartMargins[2]) / 2;
-  let chartWidth = 500
-  var chart = dc.barChart('#data-chart')
+// crossfilter variables
+// let idDim;
 
-  function updateChart (data_) {
-    let keys = d3.keys(data_[0])
-    let objects = []
-    console.debug('keys: ' + keys)
-    keys.forEach(function (e, i) {
-      if (i >= 4) {
-        if (i <= 37) {
-          console.debug('key: ' + e + ' val: ' + data_[0][keys[i]])
-          let obj = {
-            ageBand: e,
-            cnt: data_[0][keys[i]]
-          }
-          objects.push(obj)
-        }
-      }
-    })
+function loadData (file) {
+  Promise.all([
+    d3.csv('/data/tools/geodemographics/dublin_clusters.csv'),
+    d3.csv('/data/tools/geodemographics/dublin_zscores.csv')])
+ .then((data) => {
+   console.log(data[0].length)
+   console.log(data[1].length)
+    // processVariables(data);
+ })
+}
 
-    console.log('object zero: ' + JSON.stringify(objects[0]))
-    let ndx = crossfilter(objects)
-    console.log('xf: ' + ndx.size())
-    let ageDimension = ndx.dimension(function (d) {
-        // console.log("key: " + d.ageBand);
-        return d.ageBand
-      }),
-      ageGroup = ageDimension.group()
-      .reduceSum(function (d) {
-        return d.cnt
-      })
-    chart
-      .width(chartWidth)
-      .height(chartHeight)
-      .x(d3.scaleBand())
-      .xUnits(dc.units.ordinal)
-      .brushOn(false)
-      .xAxisLabel('Age Group')
-      .yAxisLabel('# Persons')
-      .dimension(ageDimension)
-      .barPadding(0.1)
-      .outerPadding(0.05)
-      .group(ageGroup)
-      .xAxis().tickValues([keys[4], keys[37]])
-    chart.render()
-  }
+let idDim // data dimension accessible by GEOGID
 
-  function style (f) {
-    // console.log("style feature "+f.properties.COUNTYNAME)
-    return {
-      fillColor: getCountyColor(f.properties.COUNTYNAME),
-      weight: 1,
-      opacity: 2,
-      color: getCountyColor(f.properties.COUNTYNAME),
-      dashArray: '1',
-      fillOpacity: 0.5
-    }
-  };
-  boundaries.addTo(mapGeodemos)
-};
+// function loadClusters (file) {
+//   //    data_.forEach(function (d) {
+//   //        d.id = +d.GEOGID.split('_')[1]; //extract the numerical part
+//   //        //corresponding to the boundary geojson
+//   //    });
+//   // console.log('Variables length = ' + data_.length)
+//   // let censusDataXF = crossfilter(data_)
+//   // idDim = censusDataXF.dimension(function (d) {
+//   //   return +d.GEOGID.split('_')[1]
+//   // })
+// }
+
+  // function formatData (data_) {
+  //   // let res = JSON.stringify(data_, null, '\n');
+  //
+  //   let keys = d3.keys(data_[0])
+  //   // Mens ages from index 4 to 37 inclusive
+  //   // 18+ from index 22
+  //   let over18Males = 0
+  //   for (let i = 22; i < 38; i += 1) {
+  //     over18Males += data_[0][keys[i]]
+  //     // console.log(keys[i]+" : "+data_[0][keys[i]]);
+  //   }
+  //   let over18Females = 0
+  //   // Female ages from index 39 to 72 inclusive
+  //   // 18+ from index 57
+  //   for (let i = 57; i < 73; i += 1) {
+  //     over18Females += data_[0][keys[i]]
+  //     // console.log(keys[i]+" : "+data_[0][keys[i]]);
+  //   }
+  //   let totalMale = data_[0]['T1_1AGETM']
+  //   let totalFemale = data_[0]['T1_1AGETF']
+  //   let textOutput1 = 'The population of this small area is <b>' +
+  //     totalMale + ' males</b> and <b>' +
+  //     totalFemale + ' females</b>, giving <b>' +
+  //     (totalMale + totalFemale) + '</b> persons in total. '
+  //   let textOutput2 = 'Of those, <b>' +
+  //     over18Males + ' men</b> and <b>' +
+  //     over18Females + ' women</b> are over 18 years of age. <br>'
+  //   return textOutput1 + textOutput2
+  // }
+  //
+  // let chartMargins = [10, 0, 30, 20]
+  // // (map size - margins)/2 map size is specified in the css for leaflet charts
+  // let chartHeight = 250 // (200 - chartMargins[0] - chartMargins[2]) / 2;
+  // let chartWidth = 500
+  // var chart = dc.barChart('#data-chart')
+  //
+  // function updateChart (data_) {
+  //   let keys = d3.keys(data_[0])
+  //   let objects = []
+  //   console.debug('keys: ' + keys)
+  //   keys.forEach(function (e, i) {
+  //     if (i >= 4) {
+  //       if (i <= 37) {
+  //         console.debug('key: ' + e + ' val: ' + data_[0][keys[i]])
+  //         let obj = {
+  //           ageBand: e,
+  //           cnt: data_[0][keys[i]]
+  //         }
+  //         objects.push(obj)
+  //       }
+  //     }
+  //   })
+  //
+  //   console.log('object zero: ' + JSON.stringify(objects[0]))
+  //   let ndx = crossfilter(objects)
+  //   console.log('xf: ' + ndx.size())
+  //   let ageDimension = ndx.dimension(function (d) {
+  //       // console.log("key: " + d.ageBand);
+  //       return d.ageBand
+  //     }),
+  //     ageGroup = ageDimension.group()
+  //     .reduceSum(function (d) {
+  //       return d.cnt
+  //     })
+  //   chart
+  //     .width(chartWidth)
+  //     .height(chartHeight)
+  //     .x(d3.scaleBand())
+  //     .xUnits(dc.units.ordinal)
+  //     .brushOn(false)
+  //     .xAxisLabel('Age Group')
+  //     .yAxisLabel('# Persons')
+  //     .dimension(ageDimension)
+  //     .barPadding(0.1)
+  //     .outerPadding(0.05)
+  //     .group(ageGroup)
+  //     .xAxis().tickValues([keys[4], keys[37]])
+  //   chart.render()
+  // }
+  //
+//   boundaries.addTo(mapGeodemos)
+// };
+
 // ['#eff3ff', '#bdd7e7', '#6baed6', '#3182bd', '#08519c']
 function getCountyColor (d) {
   return d === 'Dublin City' ? '#08519c' :
