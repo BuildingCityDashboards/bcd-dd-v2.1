4e077e91071dc465c0449c5810f4eaf4b0e38ee8
LiamOSullivan <lmosulli@tcd.ie>
2020-04-26 13:09:45 +0100

    Standardise module extensions

--- public/javascripts/modules/bcd-ui.mjs
+++ public/javascripts/modules/bcd-ui.mjs
@@ -1,3 +1,13 @@
+const populateDropdown = (id, optionsArray) => {
+  let dd = document.getElementById(id)
+  optionsArray.forEach((optionContent, i) => {
+    let o = document.createElement('option')
+    o.textContent = optionContent
+    o.value = optionContent
+    dd.appendChild(o)
+  })
+}
+
 const populateDropdownFromArray = (element, optionsArray) => {
   optionsArray.forEach((optionContent, i) => {
     let o = document.createElement('option')
