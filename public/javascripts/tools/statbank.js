29e000a03cceee8901a69d31ee19bf55f56db9a4
LiamOSullivan <lmosulli@tcd.ie>
2020-05-04 17:05:00 +0100

    Refactor module file extensions to .js to support jsdoc and generate sample doc

--- public/javascripts/tools/statbank.js
+++ public/javascripts/tools/statbank.js
@@ -1,7 +1,7 @@
-import { fetchJsonFromUrlAsync } from '../modules/bcd-async.mjs'
-import { forEachAsync } from '../modules/bcd-async.mjs'
-import { getTableMetadata } from '../modules/bcd-statbank.mjs'
-import { populateDropdownFromArray } from '../modules/bcd-ui.mjs'
+import { fetchJsonFromUrlAsync } from '../modules/bcd-async'
+import { forEachAsync } from '../modules/bcd-async'
+import { getTableMetadata } from '../modules/bcd-statbank'
+import { populateDropdownFromArray } from '../modules/bcd-ui'
 
 (async () => {
   console.log('Statbank Tool')
@@ -125,11 +125,11 @@ import { populateDropdownFromArray } from '../modules/bcd-ui.mjs'
 // let resource = fetchResource(tabletableCodes)
 // console.log(resource.length)
 
-// import JSONstat from 'https://unpkg.com/jsonstat-toolkit@1.0.8/import.mjs'
-// import { datalist } from 'https://unpkg.com/jsonstat-utils@2.5.5/export.mjs'
+// import JSONstat from 'https://unpkg.com/jsonstat-toolkit@1.0.8/import'
+// import { datalist } from 'https://unpkg.com/jsonstat-utils@2.5.5/export'
 //
-// import { populateDropdownFromArray } from './utils/bcd-ui.mjs'
-// // import { fetchStatbankTableFromUrl } from './utils/bcd-statbank.mjs'
+// import { populateDropdownFromArray } from './utils/bcd-ui'
+// // import { fetchStatbankTableFromUrl } from './utils/bcd-statbank'
 //
 // const STATBANK_BASE_URL =
 //         'https://statbank.cso.ie/StatbankServices/StatbankServices.svc/jsonservice/responseinstance/'
