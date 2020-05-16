14232cd92ca8b360ecefda4c082310b29c658312
LiamOSullivan <lmosulli@tcd.ie>
2020-05-16 12:56:33 +0100

    fix #987

--- public/javascripts/themes/demographics.js
+++ public/javascripts/themes/demographics.js
@@ -1,52 +1,55 @@
-let populationChart, outsideStateChart, houseHoldsChart, houseHoldCompositionChart;
-let population, outsideStateContent, outsideStateTT, houseHoldsContent, houseHoldsTT, houseHoldCompositionContent, houseHoldCompositionTT;
+let populationChart, outsideStateChart, houseHoldsChart, houseHoldCompositionChart
+let population, outsideStateContent, outsideStateTT, houseHoldsContent, houseHoldsTT, houseHoldCompositionContent, houseHoldCompositionTT
 
-d3.csv("../data/Demographics/CNA13.csv").then(data => {
-d3.selectAll(".chart-holder_PH").attr("class","chart-holder");
+if (document.getElementById('chart-population')) {
+  d3.csv('../data/Demographics/CNA13.csv').then(data => {
+// d3.selectAll(".chart-holder_PH").attr("class","chart-holder");
 // d3.select('#population').selectAll(".chart-holder").style("background-image", "none");
 // d3.select('#').selectAll(".chart-holder").style("background-image", "none");
 // d3.select('#').selectAll(".chart-holder").style("background-image", "none");
 // d3.select('#').selectAll(".chart-holder").style("background-color", "#000000");
-  const columnNames = data.columns.slice(2),
-    xValue = data.columns[0];
-  groupBy = data.columns[0];
-  yLabels = ["Population (000s)", "Rate %"];
 
-  const valueData = data.map(d => {
-    d.label = d.date;
-    d.date = parseYear(d.date);
-    for (var i = 0, n = columnNames.length; i < n; i++) {
-      d[columnNames[i]] = +d[columnNames[i]];
-    }
-    return d;
-  });
+    const columnNames = data.columns.slice(2)
 
-  const types = d3.nest()
+    const xValue = data.columns[0]
+    const groupBy = data.columns[0]
+    const yLabels = ['Population (000s)', 'Rate %']
+
+    const valueData = data.map(d => {
+      d.label = d.date
+      d.date = parseYear(d.date)
+      for (var i = 0, n = columnNames.length; i < n; i++) {
+        d[columnNames[i]] = +d[columnNames[i]]
+      }
+      return d
+    })
+
+    const types = d3.nest()
     .key(d => {
-      return d[groupBy];
-    }).entries(valueData);
+      return d[groupBy]
+    }).entries(valueData)
 
-  const grouping = types.map(d => d.key);
+    const grouping = types.map(d => d.key)
 
-  population = {
-    e: "#chart-population",
-    ks: grouping,
-    xV: "date",
-    yV: columnNames[0],
-    d: types,
-    tX: "Years",
-    tY: "Population",
-    ySF: "millions"
-  };
+    population = {
+      e: '#chart-population',
+      ks: grouping,
+      xV: 'date',
+      yV: columnNames[0],
+      d: types,
+      tX: 'Years',
+      tY: 'Population',
+      ySF: 'millions'
+    }
 
-  populationChart = new MultiLineChart(population);
-  populationChart.yLabels = yLabels;
-  populationChart.tickNumber = 106;
-  populationChart.drawChart();
+    populationChart = new MultiLineChart(population)
+    populationChart.yLabels = yLabels
+    populationChart.tickNumber = 106
+    populationChart.drawChart()
 
   // add the tooltip
-  populationChart.addTooltip("Year: ", "thousands", "label");
-  populationChart.showSelectedLabels([0, 16, 26, 36, 46, 56, 66, 76, 86, 96, 106]);
+    populationChart.addTooltip('Year: ', 'thousands', 'label')
+    populationChart.showSelectedLabels([0, 16, 26, 36, 46, 56, 66, 76, 86, 96, 106])
   // populationChart.showSelectedLabels([0, 16, 26, 36, 41, 46, 51, 56, 61, 69, 71, 76, 81, 86, 92, 96, 101, 106]);
 
   // d3.select(window).on("resize", function() {
@@ -55,166 +58,160 @@ d3.selectAll(".chart-holder_PH").attr("class","chart-holder");
   //   populationChart.showSelectedLabels([0, 16, 26, 36, 41, 46, 51, 56, 61, 69, 71, 76, 81, 86, 92, 96, 101, 106]);
   //
   // });
-}).catch(function(error) {
-  console.log(error);
-});
+  }).catch(function (error) {
+    console.log(error)
+  })
+}
 
-d3.csv("../data/Demographics/CNA14.csv").then(data => {
-
-  data.forEach(d => {
-    d.Dublin = +d.Dublin;
-  });
+if (document.getElementById('chart-bornOutsideStat')) {
+  d3.csv('../data/Demographics/CNA14.csv').then(data => {
+    data.forEach(d => {
+      d.Dublin = +d.Dublin
+    })
   // const femaleRateBar = new BarChart( data,"#chart-femalespermales", "date", "Dublin", "Year", "Rate");
+  }).catch(function (error) {
+    console.log(error)
+  })
 
-}).catch(function(error) {
-  console.log(error);
-});
+  d3.csv('../data/Demographics/CNA31.csv').then(data => {
+    const columnNames = data.columns.slice(2),
+      xValue = data.columns[1]
+    yLabels = ['Population (000s)']
 
-d3.csv("../data/Demographics/CNA31.csv").then(data => {
-
-  const columnNames = data.columns.slice(2),
-    xValue = data.columns[1];
-  yLabels = ["Population (000s)"];
-
-  const combinedData = d3.nest()
-    .key(function(g) {
-      return g.date;
+    const combinedData = d3.nest()
+    .key(function (g) {
+      return g.date
     })
-    .rollup(function(v) {
+    .rollup(function (v) {
       return {
-        state: d3.sum(v, function(g) {
-          return g.State;
+        state: d3.sum(v, function (g) {
+          return g.State
         }),
-        dublin: d3.sum(v, function(g) {
-          return g.Dublin;
+        dublin: d3.sum(v, function (g) {
+          return g.Dublin
         })
-      };
+      }
     })
-    .entries(data);
-  let array = [];
+    .entries(data)
+    let array = []
 
-  combinedData.forEach(d => {
-    let obj = {}
-    obj.date = d.key;
-    obj.Dublin = d.value.dublin;
-    obj.State = d.value.state;
-    array.push(obj);
-  });
+    combinedData.forEach(d => {
+      let obj = {}
+      obj.date = d.key
+      obj.Dublin = d.value.dublin
+      obj.State = d.value.state
+      array.push(obj)
+    })
 
-  outsideStateContent = {
-    e: "#chart-bornOutsideState",
-    d: array,
-    ks: columnNames,
-    xV: xValue,
-    tX: "Years",
-    tY: "Population",
-    ySF: "millions"
-  };
+    outsideStateContent = {
+      e: '#chart-bornOutsideState',
+      d: array,
+      ks: columnNames,
+      xV: xValue,
+      tX: 'Years',
+      tY: 'Population',
+      ySF: 'millions'
+    }
 
-  outsideStateTT = {
-    title: "Born Outside the State - Year:",
-    datelabel: xValue,
-    format: "thousands",
-  };
+    outsideStateTT = {
+      title: 'Born Outside the State - Year:',
+      datelabel: xValue,
+      format: 'thousands'
+    }
 
   //  for each d in combineData get the key and assign to each d in d.values
 
-  outsideStateChart = new GroupedBarChart(outsideStateContent);
+    outsideStateChart = new GroupedBarChart(outsideStateContent)
   // outsideStateChart.tickNumber = 1;
-  outsideStateChart.drawChart();
-  outsideStateChart.addTooltip(outsideStateTT);
-  outsideStateChart.showSelectedLabels([0, 2, 4, 6, 8, 10, 12, 14]);
+    outsideStateChart.drawChart()
+    outsideStateChart.addTooltip(outsideStateTT)
+    outsideStateChart.showSelectedLabels([0, 2, 4, 6, 8, 10, 12, 14])
 
   // d3.select(window).on("resize", function() {
   //   outsideStateChart.drawChart();
   //   outsideStateChart.addTooltip(outsideStateTT);
   // });
+  }).catch(function (error) {
+    console.log(error)
+  })
+}
+if (document.getElementById('chart-households')) {
+  d3.csv('../data/Demographics/CNA33.csv').then(data => {
+    const columnNames = data.columns.slice(1)
+    const xValue = data.columns[0]
 
-}).catch(function(error) {
-  console.log(error);
-});
-
-
-d3.csv("../data/Demographics/CNA33.csv").then(data => {
-  const columnNames = data.columns.slice(1);
-  const xValue = data.columns[0];
-
-  const valueData = data.map(d => {
-    for (var i = 0, n = columnNames.length; i < n; i++) {
-      d[columnNames[i]] = +d[columnNames[i]];
+    const valueData = data.map(d => {
+      for (var i = 0, n = columnNames.length; i < n; i++) {
+        d[columnNames[i]] = +d[columnNames[i]]
+      }
+      return d
+    })
+    houseHoldsContent = {
+      e: '#chart-households',
+      d: valueData,
+      ks: columnNames,
+      xV: xValue,
+      tX: 'Years',
+      tY: 'Number of Households',
+      ySF: 'millions'
     }
-    return d;
-  });
-  houseHoldsContent = {
-    e: "#chart-households",
-    d: valueData,
-    ks: columnNames,
-    xV: xValue,
-    tX: "Years",
-    tY: "Number of Households",
-    ySF: "millions"
-  };
 
-  houseHoldsTT = {
-    title: "Number of Households - Year:",
-    datelabel: xValue,
-    format: "thousands",
-  };
+    houseHoldsTT = {
+      title: 'Number of Households - Year:',
+      datelabel: xValue,
+      format: 'thousands'
+    }
 
-  houseHoldsChart = new GroupedBarChart(houseHoldsContent);
-  houseHoldsChart.drawChart();
-  houseHoldsChart.addTooltip(houseHoldsTT);
+    houseHoldsChart = new GroupedBarChart(houseHoldsContent)
+    houseHoldsChart.drawChart()
+    houseHoldsChart.addTooltip(houseHoldsTT)
 
   // d3.select(window).on("resize", function() {
   //   // houseHoldsChart.drawChart();
   //   // houseHoldsChart.addTooltip(houseHoldsTT);
   // });
+  }).catch(function (error) {
+    console.log(error)
+  })
+}
+if (document.getElementById('chart-householdComposition')) {
+  d3.csv('../data/Demographics/CNA29.csv').then(data => {
+    const columnNames = data.columns.slice(2)
+    const xValue = data.columns[0]
 
-
-
-}).catch(function(error) {
-  console.log(error);
-});
-
-
-d3.csv("../data/Demographics/CNA29.csv").then(data => {
-
-  const columnNames = data.columns.slice(2);
-  const xValue = data.columns[0];
-
-  const valueData = data.map(d => {
-    for (var i = 0, n = columnNames.length; i < n; i++) {
-      d[columnNames[i]] = +d[columnNames[i]];
+    const valueData = data.map(d => {
+      for (var i = 0, n = columnNames.length; i < n; i++) {
+        d[columnNames[i]] = +d[columnNames[i]]
+      }
+      return d
+    })
+    houseHoldCompositionContent = {
+      e: '#chart-householdComposition',
+      d: valueData,
+      ks: columnNames,
+      xV: xValue,
+      tX: 'Person per Household',
+      tY: 'Number of Households',
+      ySF: 'millions'
     }
-    return d;
-  });
-  houseHoldCompositionContent = {
-    e: "#chart-householdComposition",
-    d: valueData,
-    ks: columnNames,
-    xV: xValue,
-    tX: "Person per Household",
-    tY: "Number of Households",
-    ySF: "millions"
-  };
 
-  houseHoldCompositionTT = {
-    title: "Person per Household:",
-    datelabel: xValue,
-    format: "thousands",
-  };
+    houseHoldCompositionTT = {
+      title: 'Person per Household:',
+      datelabel: xValue,
+      format: 'thousands'
+    }
 
-  houseHoldCompositionChart = new GroupedBarChart(houseHoldCompositionContent);
-  houseHoldCompositionChart.drawChart();
-  houseHoldCompositionChart.addTooltip(houseHoldCompositionTT);
-  houseHoldCompositionChart.hideRate(true);
+    houseHoldCompositionChart = new GroupedBarChart(houseHoldCompositionContent)
+    houseHoldCompositionChart.drawChart()
+    houseHoldCompositionChart.addTooltip(houseHoldCompositionTT)
+    houseHoldCompositionChart.hideRate(true)
 
   // d3.select(window).on("resize", function() {
   //   console.log("Resize");
   //   houseHoldCompositionChart.drawChart();
   //   houseHoldCompositionChart.addTooltip(houseHoldCompositionTT);
   // });
-
-}).catch(function(error) {
-  console.log(error);
-});
+  }).catch(function (error) {
+    console.log(error)
+  })
+}
