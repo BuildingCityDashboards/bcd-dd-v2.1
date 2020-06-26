a94f09f9619c89cb95f52bb846c318d5f55cbe6f
LiamOSullivan <lmosulli@tcd.ie>
2020-06-26 16:38:22 +0100

    add pt 3 fig 1

---
+++ public/javascripts/stories/housing-scrolly/story-housing-3.js
@@ -0,0 +1,165 @@
+/**
+ *
+ *
+ * @param {}
+ * @return {}
+ *
+ *
+ */
+
+// TODO:
+// load page metadata for n narrative objects and n charts
+// load n chart plot objects into array
+// draw chart based on TwoStep interaction
+
+(async () => {
+  const CHART_STICKY_ELEMENT = 'chart-sticky-housing'
+  const MAP_STICKY_ELEMENT = 'map-sticky-housing'
+// init a blank plot
+// TODO: replace with spinner/ loading progress
+  // Plotly.newPlot('chart-sticky-housing', {}, {}, {})
+
+  /**
+   * Styling on Plotly Laout options object
+   *
+   * @param { Object } po
+   * @return { Object } po
+   *
+   *
+   */
+
+  const stylePlotlyLayout = po => {
+    if (po.hasOwnProperty('layout')) {
+      // console.log(i + ' has layout')
+      po.layout.modebar = {
+        'bgcolor': 'rgba(0, 0, 0, 0)'
+      }
+    }
+    return po
+  }
+
+  // TODO: this is dumb, but must wait for a genralised function to return plot objects
+  let plotObjects = []
+
+  let plotObject = {}
+  let map = {} // initialise
+  plotObject = await getPlotObjectFig1()
+  stylePlotlyLayout(plotObject)
+  // console.log('plot 1 loaded')
+  plotObjects.push(plotObject)
+
+  const drawPlot = async (event) => {
+    // console.log('Waypoint ' + JSON.stringify(event) + ' triggered')
+    let chartSticky = document.getElementById(CHART_STICKY_ELEMENT)
+    let mapSticky = document.getElementById(MAP_STICKY_ELEMENT)
+    let chartState = chartSticky.getAttribute('data-status')
+    let mapState = mapSticky.getAttribute('data-status')
+    // console.log(chartState)
+    // on trigger at index, get state based on index
+    console.log(event.index + ' map state: ' + mapState + ' c state: ' + chartState)
+    // TODO: don't use indexes of map elements here in this way
+
+    // indexes of map elements
+    if (event.index == 2) {
+      if (mapState === 'hidden') {
+        console.log('draw map and show')
+        // console.log('map')
+        // console.log(map)
+        // hide current chart
+        chartSticky.removeAttribute('data-status')
+        chartSticky.setAttribute('data-status', 'hidden')
+        chartSticky.style.display = 'none'
+
+        // unhide map
+        mapSticky.style.display = 'block'
+        if (map.hasOwnProperty('options')) {
+          map.invalidateSize(true)
+          mapSticky.removeAttribute('data-status')
+          mapSticky.setAttribute('data-status', 'shown')
+        }
+      }
+    } else if (event.index >= 0 && event.index < plotObjects.length) {
+      if (mapState === 'shown') {
+        console.log('hide map')
+        // console.log('map')
+        // console.log(map)
+        // hide current map
+        mapSticky.removeAttribute('data-status')
+        mapSticky.setAttribute('data-status', 'hidden')
+        mapSticky.style.display = 'none'
+      }
+
+      if (chartState == 'hidden') {
+        console.log('draw chart and show')
+        chartSticky.style.display = 'block'
+        if (plotObjects[event.index].hasOwnProperty('layout')) {
+          Plotly.newPlot(CHART_STICKY_ELEMENT, plotObjects[event.index].traces, plotObjects[event.index].layout, plotObjects[event.index].options)
+          // if (event.index == 1) {
+          //   afterplotFixesFig2(CHART_STICKY_ELEMENT)
+          // } else if (event.index == 6) {
+          //   afterplotFixesFig7(CHART_STICKY_ELEMENT)
+          // }
+          chartSticky.removeAttribute('data-status')
+          chartSticky.setAttribute('data-status', 'shown')
+        }
+      } else if (chartState == 'shown') {
+        chartSticky.removeAttribute('data-status')
+        chartSticky.setAttribute('data-status', 'hidden')
+        if (plotObjects[event.index].hasOwnProperty('layout')) {
+          Plotly.newPlot(CHART_STICKY_ELEMENT, plotObjects[event.index].traces, plotObjects[event.index].layout, plotObjects[event.index].options)
+          // if (event.index == ) {
+          //   afterplotFixesFig2(CHART_STICKY_ELEMENT)
+          // } else if (event.index == 6) {
+          //   afterplotFixesFig7(CHART_STICKY_ELEMENT)
+          // }
+          chartSticky.removeAttribute('data-status')
+          chartSticky.setAttribute('data-status', 'shown')
+        }
+      }
+    }
+  }
+
+  const ts = new TwoStep({
+    elements: document.querySelectorAll('.narrative-item'),
+    onChange: drawPlot,
+    stick: document.querySelector('.rightcol')
+  })
+
+  plotObject = await getPlotObjectFig2()
+  // console.log('plot 2 loaded')
+  stylePlotlyLayout(plotObject)
+  plotObjects.push(plotObject)
+  map = await getMapFig3()
+  // console.log('map fig 3 loaded')
+  plotObjects.push(map)
+  plotObject = await getPlotObjectFig4()
+  // console.log('plot 4 loaded')
+  console.log(plotObject)
+  stylePlotlyLayout(plotObject)
+  plotObjects.push(plotObject)
+  plotObject = await getPlotObjectFig5()
+  // console.log('plot 5 loaded')
+  stylePlotlyLayout(plotObject)
+  plotObjects.push(plotObject)
+
+  plotObjects.push('Conclusion')
+  plotObjects.push('Read more')
+  plotObjects.push('Acknowledgement')
+  plotObjects.push('References')
+
+// TODO: remove jQ
+  function checkSize () {
+    // if desktop...
+    if ($(window).width() > 700) {
+      ts.enable()
+    // if mobile...
+    } else {
+      ts.disable()
+    }
+  }
+
+// Check current screen size
+// and set up event listener for future changes
+  checkSize()
+  $(window).resize(checkSize)
+})()
--- public/javascripts/stories/housing-scrolly/story-housing3-fig1.js
+++ public/javascripts/stories/housing-scrolly/story-housing3-fig1.js
@@ -1,95 +1,99 @@
-//Options for chart
-const srcPathFig1 = "../data/Stories/Housing/part_3/processed/ave_monthly_rent.csv";
-let titleFig1 = "Average Price of Rent in Euros by Quarter-Year 2007-2018";
-const divIDFig1 = "rent-prices-chart";
+const getPlotObjectFig1 = async function () {
+// Options for chart
+  const srcPathFig1 = '../data/Stories/Housing/part_3/processed/ave_monthly_rent.csv'
+  let titleFig1 = 'Average Price of Rent in Euros by Quarter-Year 2007-2018'
+  const divIDFig1 = 'rent-prices-chart'
 
-d3.csv(srcPathFig1)
-  .then((data) => {
+  let data = await d3.csv(srcPathFig1)
 
     // console.log(data);
 
-    let tracesFig1 = [];
-    tracesFig1.push(getTrace(data, "date", "Dublin"));
-    tracesFig1.push(getTrace(data, "date", "State excl. Dublin"));
+  let traces = []
+  traces.push(getTrace(data, 'date', 'Dublin'))
+  traces.push(getTrace(data, 'date', 'State excl. Dublin'))
 
-    function getTrace(data, xVar, yVar) {
-      let trace = Object.assign({}, TRACES_DEFAULT);
-      trace.name = yVar;
-      trace.visible = true;
-      //trace.hoverinfo = 'y';
-      trace.x = data.map((x) => {
-        return x[xVar];
-      });
-      trace.y = data.map((y) => {
-        // console.log(y[yVar]);
-        return y[yVar];
-      });
-      // trace.connectgaps = true;
-      trace.mode = 'lines+markers';
-      // trace.name === 'state' ? trace.visible = true : trace.visible = true;
-      trace.marker = Object.assign({}, TRACES_DEFAULT.marker);
-      REGIONS_ORDERED_DUBLIN.includes(trace.name) ? trace.opacity = 1.0 : trace.opacity = 0.5;
-      trace.marker.color = CHART_COLORS_BY_REGION[trace.name] || 'grey';
-      return trace;
-    }
-
-    //Set layout options
-    let layout = Object.assign({}, MULTILINE_CHART_LAYOUT);
-    layout.title = Object.assign({}, MULTILINE_CHART_LAYOUT.title);
-    //layout.title.text = titleFig1;
-    layout.showlegend = false;
-    layout.xaxis = Object.assign({}, MULTILINE_CHART_LAYOUT.xaxis);
-    layout.xaxis.nticks = 7;
-    layout.xaxis.title = 'Quarter-Year';
-    layout.xaxis.range = [-0.2, 42.2];
-    layout.yaxis = Object.assign({}, MULTILINE_CHART_LAYOUT.yaxis);
-    layout.yaxis.range = [1, 2000];
-    // layout.yaxis.visible = false;
-    layout.yaxis.title = '€';
-    layout.margin = Object.assign({}, MULTILINE_CHART_LAYOUT.margin);
-    layout.margin = {
-      l: 10,
-      r: 150,
-      t: 0, //button row
-      b: 40
-    };
-
-    let annotations = [];
-    tracesFig1.forEach((trace, i) => {
-      // console.log("trace: " + JSON.stringify(trace));
-      let annotation = Object.assign({}, ANNOTATIONS_DEFAULT);
-      annotation.x = trace.x[trace.x.length - 1];
-      annotation.y = trace.y[trace.y.length - 1];
-      annotation.font = Object.assign({}, ANNOTATIONS_DEFAULT.font);
-      annotation.font.color = CHART_COLORS_BY_REGION[trace.name] || 'grey'; //Is this order smae as fetching from object in trace?
-      annotation.text = trace.name;
-      annotations.push(annotation);
-
+  function getTrace (data, xVar, yVar) {
+    let trace = Object.assign({}, TRACES_DEFAULT)
+    trace.name = yVar
+    trace.visible = true
+      // trace.hoverinfo = 'y';
+    trace.x = data.map((x) => {
+      return x[xVar]
     })
+    trace.y = data.map((y) => {
+        // console.log(y[yVar]);
+      return y[yVar]
+    })
+      // trace.connectgaps = true;
+    trace.mode = 'lines+markers'
+      // trace.name === 'state' ? trace.visible = true : trace.visible = true;
+    trace.marker = Object.assign({}, TRACES_DEFAULT.marker)
+    REGIONS_ORDERED_DUBLIN.includes(trace.name) ? trace.opacity = 1.0 : trace.opacity = 0.5
+    trace.marker.color = CHART_COLORS_BY_REGION[trace.name] || 'grey'
+    return trace
+  }
+
+    // Set layout options
+  let layout = Object.assign({}, MULTILINE_CHART_LAYOUT)
+  layout.title = Object.assign({}, MULTILINE_CHART_LAYOUT.title)
+    // layout.title.text = titleFig1;
+  layout.showlegend = false
+  layout.xaxis = Object.assign({}, MULTILINE_CHART_LAYOUT.xaxis)
+  layout.xaxis.nticks = 7
+  layout.xaxis.title = 'Quarter-Year'
+  layout.xaxis.range = [-0.2, 42.2]
+  layout.yaxis = Object.assign({}, MULTILINE_CHART_LAYOUT.yaxis)
+  layout.yaxis.range = [1, 2000]
+    // layout.yaxis.visible = false;
+  layout.yaxis.title = '€'
+  layout.margin = Object.assign({}, MULTILINE_CHART_LAYOUT.margin)
+  layout.margin = {
+    l: 10,
+    r: 150,
+    t: 0, // button row
+    b: 40
+  }
+
+  let annotations = []
+  traces.forEach((trace, i) => {
+      // console.log("trace: " + JSON.stringify(trace));
+    let annotation = Object.assign({}, ANNOTATIONS_DEFAULT)
+    annotation.x = trace.x[trace.x.length - 1]
+    annotation.y = trace.y[trace.y.length - 1]
+    annotation.font = Object.assign({}, ANNOTATIONS_DEFAULT.font)
+    annotation.font.color = CHART_COLORS_BY_REGION[trace.name] || 'grey' // Is this order smae as fetching from object in trace?
+    annotation.text = trace.name
+    annotations.push(annotation)
+  })
     // annotations[1].y = annotations[0].y + annotations[1].y
 
-    layout.annotations = annotations;
+  layout.annotations = annotations
 
-    Plotly.newPlot(divIDFig1, tracesFig1, layout, {
-      modeBar: {
-        orientation: 'v',
-        bgcolor: 'black',
-        color: null,
-        activecolor: null
-      },
-      modeBarButtons: MULTILINE_CHART_MODE_BAR_BUTTONS_TO_INCLUDE,
-      displayModeBar: true,
-      displaylogo: false,
-      showSendToCloud: false,
-      responsive: true,
-      toImageButtonOptions: {
-        filename: 'Dublin Dashboard - ' + titleFig1,
-        width: null,
-        height: null,
-        format: 'png'
-      }
-    });
-  }) //end of then
-  .catch(function(err) {
-    console.log("Error loading file:\n " + err)
-  });
+  let options = {
+    modeBar: {
+      orientation: 'v',
+      bgcolor: 'black',
+      color: null,
+      activecolor: null
+    },
+    modeBarButtons: MULTILINE_CHART_MODE_BAR_BUTTONS_TO_INCLUDE,
+    displayModeBar: true,
+    displaylogo: false,
+    showSendToCloud: false,
+    responsive: true,
+    toImageButtonOptions: {
+      filename: 'Dublin Dashboard - ' + titleFig1,
+      width: null,
+      height: null,
+      format: 'png'
+    }
+  }
+
+  const plotObject = {
+    traces: traces,
+    layout: layout,
+    options: options
+  }
+  // console.log(plotObject)
+  return plotObject
+}
