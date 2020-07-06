e2dcb2d34cdc4eccedf148b18e8f3d1268f554c5
Bashir Shalaik <bsalaik2010@gmail.com>
2020-07-06 20:17:13 +0100

    Trying to solve merge conflicts

--- public/javascripts/queries/geodemos.js
+++ public/javascripts/queries/geodemos.js
@@ -53,7 +53,11 @@ let cartoDb_Dark = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/
 let cartoDb_Lite = 'https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png';
 let CARTODB_POSITRON = 'https://{s}.basemaps.cartocdn.com/rastertiles/light_nolabels/{z}/{x}/{y}.png';
 let CARTODB_ATTRIBUTION = '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, © <a href="https://carto.com/">CartoDB </a> contributors'
-
+let columnData ={}
+let punti_mappa = []
+let myTestArray=[]
+let cov=0;
+//let soc_eco_val=0
 //let mapGeodemos = new L.Map('map-geodemos')
 
 
@@ -96,7 +100,7 @@ let naStyle = {
 let mapLayers = getEmptyLayersArray(7)
 let naLayer = L.geoJSON(null, {
   style: naStyle,
-  //onEachFeature: onEachFeature
+  onEachFeature: onEachFeature
 }) //layer for 'NA' data
 // mapGeodemos.addLayer(mapLayers[0])
 
@@ -104,18 +108,25 @@ let traces = []
 let layout = {}
 let tracesIndx = []
 let hmlayout = {}
+let columnNames={}
+let tarry=[]
+
 d3.csv('/data/tools/geodemographics/dublin_zscores.csv')
 .then((zScores)=>{
 
-  let columnNames = Object.keys(zScores[0]).reverse();
+
+  //columnNames = Object.keys(zScores[0]).reverse();
+  columnNames = Object.keys(zScores[0]);
   //const zScores2=zScores.reverse();
   columnNames = columnNames.filter(e=>e!=='cluster')
+// alert(zScores)
   zScores.forEach((row, i) => {
+  
     let trace = Object.assign({}, TRACES_DEFAULT);
     trace.type = 'bar'
     trace.orientation = 'h'
     trace.marker = {
-      color: getLayerColor(i), // lines + markers, defaults to colorway
+    color: getLayerColor(i), // lines + markers, defaults to colorway
     }
    
     trace.x = columnNames.map( name => {
@@ -186,15 +197,13 @@ Plotly.newPlot('chart-geodemos', [traces[0]], layout, {
   // }
   })
   //TrackingArray.push(1)
-  updateGroupTxt(1);
+  //updateGroupTxt(1);
   //getzscorstat(0)
     
 }) //end then
 
   
-
-
-
+let soc_eco_val=0;
 loadData()
 function loadData (file) {
   d3.csv('/data/tools/geodemographics/dublin_clusters_sa_cluster.csv')
@@ -208,9 +217,9 @@ function loadData (file) {
 }
 
 async function loadSmallAreas (lookup) {
+  // alert(JSON.stringify(lookup))
   let features = []
-// Incrementally load boundaries for each LA to maps
-// signify when finished all
+
   let dataBase = '/data/tools/census2016/'
   let dcc0 = 'DCC_SA_0.geojson'
   let dcc1 = 'DCC_SA_1.geojson'
@@ -219,17 +228,25 @@ async function loadSmallAreas (lookup) {
   let pDCC0 = d3.json(dataBase + dcc0)
   let pDCC1 = d3.json(dataBase + dcc1)
   let pDCC2 = d3.json(dataBase + dcc2)
-// DCC
+
+
   let dccSAs = await Promise.all([pDCC0, pDCC1, pDCC2]) // yields an array of 3 feature collections
-  // need to extract each feature and lookup its group, adding to appropriate layer
+  
+  
   dccSAs.forEach(sas => {
     // updateMap(sas)
+    
     sas.features.forEach(sa => {
       try{
         let groupNo = lookup[sa.properties.SMALL_AREA]
         sa.properties.groupnumber= groupNo
+        
+       
+        
+        //alert(JSON.stringify(sa.geometry.coordinates[0][1]),sa.properties.groupColor)
         addFeatureToLayer(sa, parseInt(groupNo) - 1) // feature, layer index
-
+        //var punti_mappa = geoJson2heat(sa,Varval)
+        //myTestArray.push(sa.geometry.coordinates)
       }
       catch{
         console.warn(`Error on lookup for sa. Adding to NA layer \n ${JSON.stringify(sa)} `)
@@ -237,10 +254,17 @@ async function loadSmallAreas (lookup) {
         addFeatureToLayer(sa, 'NA') //Additional layer for NA sas
       }
       // console.log(layerNo)
+      
+    
     })
+    //alert(JSON.stringify(sas.features))
   })
-
-
+ // for (let u=0; u < myTestArray.length; u++)
+ // {
+    //alert(parseFloat(myTestArray[1]))
+  //}
+  
+//alert(JSON.stringify(dccSAs))
   
 // Fingal, DL/R, SDCC
   let fcc = 'FCC_SA_0.geojson'
@@ -257,26 +281,33 @@ async function loadSmallAreas (lookup) {
       try{
         let groupNo = lookup[sa.properties.SMALL_AREA]
         sa.properties.groupnumber= groupNo
+        
+        //let Varval = traces[groupNo-1].x[soc_eco_val];
+        // alert(groupNo)
+        //alert(groupNo + '----' + traces[groupNo].x[0])
+        //sa.properties.groupColor= Varval
         addFeatureToLayer(sa, parseInt(groupNo) - 1) // feature, layer index
-
+       
       }
       catch{
         console.warn(`Error on lookup for sa. Adding to NA layer \n ${JSON.stringify(sa)} `)
         sa.properties.groupnumber= 'NA'
         addFeatureToLayer(sa, 'NA') //Additional layer for NA sas
       }
-      // console.log(layerNo)
-    })
-  })
-  //Set initial layer view
+     
+})
+})
+//var punti_mappa =  geoJson2heat(sa,Varval)
   mapGeodemos.addLayer(mapLayers[0])
+  updateGroupTxt(1);
 }
 
 function getEmptyLayersArray (total) {
   let layersArr = []
   for (let i = 0; i < total; i += 1) {
     layersArr.push(L.geoJSON(null, {
-      style: getLayerStyle(i),
+//      style: subteStyle,
+      //style: getLayerStyle(i),
       onEachFeature: onEachFeature
       // filter: filterInitialView
     })
@@ -285,28 +316,67 @@ function getEmptyLayersArray (total) {
   return layersArr
 }
 
+
+
+
+
+
+function subteStyle(feature) {
+  return {
+    color: getFColor(feature.properties.groupColor),
+    weight: 0.2,
+    opacity: .5,
+    fillColor: getFColor(feature.properties.groupColor),
+    dashArray: '3',
+		fillOpacity: .6,
+  };
+}    
+
+
 function addFeatureToLayer (feature, layerNo) {
+  //alert(JSON.stringify(feature))
+  //alert(geoJson2heat(feature,feature.properties.groupColor))
+ 
+ 
   if (layerNo ==='NA'){
     naLayer.addData(feature)
     mapGeodemos.addLayer(naLayer)
   }
   else{
   mapLayers[layerNo].addData(feature)
-
+ //punti_mappa =  geoJson2heat(mapLayers[layerNo],0.8)
+ // alert(punti_mappa)
+ //alert(geoJson2heat(mapLayers[layerNo],feature.properties.groupColor))
   }
+ // alert(geoJson2heat(mapLayers[layerNo],feature.properties.groupColor))
 }
 
+//alert(geoJson2heat(mapLayers[0],feature.properties.groupColor))
+//alert(punti_mappa)
 function getLayerStyle (index) {
   // console.log("style feature "+f.properties.COUNTYNAME)
   return {
-    fillColor: getLayerColor(index),
-    weight: 1,
-    opacity: 2,
-    color: getLayerColor(index),
-    dashArray: '1',
-    fillOpacity: 0.5
+    //fillColor: getLayerColor(index),
+    //weight: 0.3,
+    //opacity: 0.3,
+    //color: getLayerColor(index),
+    //dashArray: '1',
+    //fillOpacity: 0.9
   }
 }
+
+function getLayerStyle2 (index) {
+  // console.log("style feature "+f.properties.COUNTYNAME)
+  return {
+    fillColor: getFColor(index),
+    weight: 0.3,
+    opacity: 0.3,
+    color: getFColor(index),
+    dashArray: '1',
+    fillOpacity: 0.9
+  }
+}
+
 
 function getLayerColor (index) {
   return GEODEMOS_COLORWAY[index]
@@ -323,6 +393,23 @@ function updateGroupTxt(no)
   
   })
 }
+
+
+
+
+
+function getFColor(d) {
+  return  d > 2.0 ? '#c02626' :
+          d > 1.5 ? '#d9c01c' :
+          d > 1.0 ? '#d99a1c' :       
+          d > 0.5 ? '#d9871c':
+          d == 0 ? '#d9771c' :
+          d < 0 ? '#d95b1c' :
+          d > -0.5  ? '#d93f1c' :
+          d > -1.0  ? '#d9221c' :
+                   '#b9d91c';
+}
+
 
 
 
@@ -393,19 +480,97 @@ function getMed(array)
   
 }
 
+
+
+
+
+//traces[feature.properties.groupnumber].x[strUser]
+
+let value=0
+let text=''
+//let cov=0;
+d3.select('#seco').on('change' , function()
+{
+ 
+  //soc_eco_val
+  var e = document.getElementById("seco");
+  var value = e.options[e.selectedIndex].value;
+  var text = e.options[e.selectedIndex].text;
+  
+  mapLayers.forEach((l,k) =>{
+    mapGeodemos.removeLayer(l)
+    //if(!mapGeodemos.hasLayer(l)){
+    let mlay=mapLayers[k]
+     cov=traces[k].x[value];
+    //let feature.properties.groupColor=cov
+    mapGeodemos.addLayer(l)
+    mlay.setStyle({color: '#da0101',//getFColor(cov),
+    fillColor: getFColor(cov),
+    weight: 1, //parseFloat(cov),
+    opacity: parseFloat(cov),
+      
+      dashArray: '1',
+      fillOpacity: 0.5 //parseFloat(cov)
+      
+    
+      //fillColor: 'grey',
+      // weight: 1,
+      // opacity: 2,
+      // color: 'grey',
+      // dashArray: '1',
+      // fillOpacity: 0.5
+
+    });
+   
+//  }
+})
+//addheatmap() 
+//alert('yes1' + value)
+addheatmap_Sv(value,text)
+updateGroupTxt('all')
+}
+)
 function onEachFeature (feature, layer) {
-  layer.bindPopup(
+  //let e = document.getElementById("seco");
+  
+  //let strUser = e.options[e.selectedIndex].value;
+
+  //var e = document.getElementById("seco");
+  //var value = e.options[e.selectedIndex].value;
+  //var text = e.options[e.selectedIndex].text;
+  //alert(typeof parseInt(feature.properties.groupnumber))
+  //let idx=feature.properties.groupnumber
+  //if (idx !='NA')
+  //{
+  //  idx=parseInt(feature.properties.groupnumber)
+    //alert(idx + '---' + typeof(idx))
+    //let mlay=mapLayers[ln]
+   //let cov2=traces[idx].x[value];
+  //}
+  //let mlay=mapLayers[idx]
+    layer.bindPopup(
                 '<p><b>' + feature.properties.EDNAME + '</b></p>' +
+                '<p><b>' + cov + '<p><b>' +
+              //  '<p><b>' + cov2 + '</b></p>' +
                 '<p>' + feature.properties.COUNTYNAME + '</p>' +
                 '<p>SA ' + feature.properties.SMALL_AREA + '</p>'+
                 '<p>Group ' + feature.properties.groupnumber + '</p>'
+                
                 )
-        // bind click
+    //     }     // bind click
   layer.on({
     click: function () {
     }
   })
 }
+//}
+
+
+
+
+//)
+
+
 
 
 
@@ -496,7 +661,7 @@ d3.select('#group-buttons').selectAll('img').on('click' ,function(){
 
    if (layerNo === 'all') {// 'all' && cb.attr("src")=='/images/icons/Icon_eye_selected.svg') {
  //alert('all') 
- 
+//alert(parseInt(soc_eco_val)) 
  for(let i=1; i<8; i++)
  {
   let myimg2=document.getElementById(i)    
@@ -514,16 +679,62 @@ let srrc = cb.attr("src");
     
     //if (srrc=="/images/icons/Icon_eye_selected.svg")
     //{
-    mapLayers.forEach( l =>{
+    mapLayers.forEach((l,k) =>{
+    //alert( soc_eco_val+ '---'+ traces[k].x[soc_eco_val])
     if(!mapGeodemos.hasLayer(l)){
-    mapGeodemos.addLayer(l)
-    //Plotly.react('chart-geodemos', [traces,traces[myv]] ,layout)
-  }})
-
-
+    let mlay=mapLayers[k]
+    let cov=traces[k].x[soc_eco_val];
     
-  addheatmap()    
-  updateGroupTxt('all')
+    //alert(selText)
+    
+    //l.popup.setContent('<br> <p> <b>'+ cov);
+    //mlay.getPopup().update(+ 'sssd' );
+    // mlay.bindPopup(
+    //   + '<p><b> val2' + cov + '</b></p>' 
+      
+    //   )
+    mapGeodemos.addLayer(mlay)
+
+
+    //mlay.setStyle(getLayerStyle2(cov))
+     mlay.setStyle({color: getFColor(cov),
+      fillColor: getFColor(cov),
+    weight: 0.3,
+    opacity: 0.3,
+    color: getFColor(cov),
+    dashArray: '1',
+    fillOpacity: 0.9
+    
+    
+    }
+     
+    
+    );
+    //Plotly.react('chart-geodemos', [traces,traces[myv]] ,layout)
+    //alert(traces[k].x[soc_eco_val]);
+    //alert(layerNo + '---' + soc_eco_val)
+  }})
+// let tto=7
+//   function getl(tto) {
+//     //let layersArr = []
+//     for (let i = 0; i < tto; i += 1) {
+//       //layersArr.push(L.geoJSON(null, {
+//         //style: subteStyle,
+//         //style: getLayerStyle(i),
+//         //onEachFeature: onEachFeature
+//     alert(traces[i].x + '---' + soc_eco_val)
+//         // filter: filterInitialView
+//       }
+      
+//     }
+    //return layersArr
+  //}
+
+
+
+  //getl()    
+  //addheatmap()    
+  //updateGroupTxt('all')
 
   //let seim =document.getElementById("1")
       
@@ -606,6 +817,10 @@ let srrc = cb.attr("src");
 //)
 //})
 
+
+
+
+
 function addheatmap ()
 {
   let GroupsArray=['Group1', 'Group2', 'Group3', 'Group4', 'Group5','Group6','Group7']
@@ -676,10 +891,11 @@ function addheatmap ()
     // number of columns in data rows as in header row
     for(let j=0; j<numberOfColumns; j++) {
       columnData[j].push((rowData[j]));
-    }
+    
+    
   }
-  
-  
+}
+//alert(columnData[1][0])
   
   let data = [
       {
@@ -687,11 +903,35 @@ function addheatmap ()
           x: GroupsArray,
           y: header.split(','),
           type: 'heatmap',
-          hoverinfo: true,
-          hoverongaps: true,
-          showticker: true, 
-          colorscale: [[0, 'rgb(166,206,227)'], [0.25, 'rgb(31,120,180)'], [0.45, 'rgb(178,223,138)'], [0.65, 'rgb(51,160,44)'], [0.85, 'rgb(251,154,153)'], [1, 'rgb(227,26,28)']]
-          ,linecolor: 'White',
+         
+          //colorscale: [[0, '#800026'], [0.25, 'rgb(31,120,180)'], [0.45, 'rgb(178,223,138)'], [0.65, 'rgb(51,160,44)'], [0.85, 'rgb(251,154,153)'], [1, 'rgb(227,26,28)']]
+         
+          
+          colorscale: [
+
+            // Let first 10% (0.1) of the values have color rgb(0, 0, 0)
+            
+
+           
+          
+            
+            // Let values between 10-20% of the min and max of z
+            // have color rgb(20, 20, 20)
+            
+                    [0.1, '#FFEDA0'],
+                    [0.7, '#FFEDA0'],
+            
+            // Values between 20-30% of the min and max of z
+            //have color rgb(40, 40, 40)
+            
+                    [0.7, '#BD0026'],
+                    [1.0, '#BD0026'],
+            
+                    
+                    
+                ]
+
+
          
       }  
           
@@ -700,5 +940,131 @@ function addheatmap ()
       Plotly.newPlot('chart-geodemos', data, hmlayout);
 
 })
+
+}
+
+
+
+function addheatmap_Sv (value,text)
+{
+  //alert('yes2' + value)
+  let GroupsArray=['Group1', 'Group2', 'Group3', 'Group4', 'Group5','Group6','Group7']
+  hmlayout = Object.assign({}, ROW_CHART_LAYOUT);
+  //layout.mode = 'bars'
+  hmlayout.height = 300
+  hmlayout.width = 360
+  //layout.barmode = 'group';
+  hmlayout.colorway = GEODEMOS_COLORWAY
+  hmlayout.title = Object.assign({}, ROW_CHART_LAYOUT.title);
+  hmlayout.title.text = 'Variables Value Distribution (z-scores)';
+  hmlayout.showlegend = false;
+  hmlayout.legend = Object.assign({}, ROW_CHART_LAYOUT.legend);
+  hmlayout.legend.xanchor = 'right';
+  hmlayout.legend.y = 0.1;
+  hmlayout.legend.traceorder = 'reversed';
+  hmlayout.xaxis = Object.assign({}, ROW_CHART_LAYOUT.xaxis);
+  hmlayout.xaxis.title = "";
+  //hmlayout.xaxis.range = [-2, 2]
+  hmlayout.yaxis = Object.assign({}, ROW_CHART_LAYOUT.yaxis);
+  hmlayout.yaxis.tickfont ={
+    family: 'PT Sans',
+    size: 10,
+    color: '#313131'
+  }
+  hmlayout.yaxis.titlefont = Object.assign({}, ROW_CHART_LAYOUT.yaxis.titlefont);
+  hmlayout.yaxis.titlefont.size = 16; //bug? need to call this
+  hmlayout.yaxis.title = Object.assign({}, ROW_CHART_LAYOUT.yaxis.title);
+  //-hmlayout.hovermode ='closest';
+  //-hmlayout.hoverinfo = "z";
+  //-hmlayout.domain =[0.85,0.9];
+  hmlayout.yaxis.title = '';
+  hmlayout.margin = Object.assign({}, ROW_CHART_LAYOUT.margin)
+  hmlayout.margin = {
+    l: 20,
+    r: 20, //annotations space
+    t: 20,
+    b: 0
+    
+  };
+  
+
+
+  d3.text('/data/tools/geodemographics/dublin_zscores.csv')
+  .then((zScores)=>{
+    let newCsv = zScores.split('\n').map(function(line) {
+      let columns = line.split(','); // get the columns
+      columns.splice(0, 1); // remove total column
+      return columns;
+  }).join('\n');
+
+  const rows = newCsv.split('\n');
+  //alert(rows)
+  // get the first row as header
+  const header = rows.shift(); //.revers();
+  //alert(header)
+  //const header = columnNames;
+  const numberOfColumns = header.split(',').length
+  
+  // initialize 2D-array with a fixed size
+  const columnData = [...Array(numberOfColumns)].map(item => new Array());
+  
+  for(let i=0; i<rows.length; i++) {
+    let row = rows[i];
+    let rowData = row.split(',');
+  
+   
+    for(let j=0; j<numberOfColumns; j++) {
+      columnData[j].push((rowData[j]));
+    
+    
+  }
+}
+let zArray=[]
+for (let r=0;r<7;r++)
+{
+//alert(columnData[value][r])
+zArray.push(columnData[value][r])
+} 
+  let data = [
+      {
+          z: [zArray], //columnData,
+          //x: GroupsArray,
+          //y: header.split(','),
+          //z: [[2.053633391,0.261867764,-0.412739154,-0.588462387,-0.466565008,-0.694338304,0.894616436]],
+          x: GroupsArray, // ['', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+          y: [text],
+          type: 'heatmap',
+          hoverongaps: false,
+          
+         
+          //colorscale: [[0, '#800026'], [0.25, 'rgb(31,120,180)'], [0.45, 'rgb(178,223,138)'], [0.65, 'rgb(51,160,44)'], [0.85, 'rgb(251,154,153)'], [1, 'rgb(227,26,28)']]
+          // return  d == 0 ? 'rgb(165,0,38)' :
+          // d < 0 ? 'rgb(215,48,39)' :
+          // d > 0  ? 'rgb(10,10,10)' :
+          //          'rgb(240,25,76)';
+          
+          colorscale: [
+
+            [0.1, 'rgb(165,0,38)'],
+            [0.9, 'rgb(165,0,38)'],
+            
+            [0.9, 'rgb(220,0,138)'],
+            [2.0, 'rgb(220,0,138)']
+            
+            [-0.5, 'rgb(10,10,10)'],
+            [0.5, 'rgb(10,10,10)'],
+
+          ]
+
+
+         
+      }  
+          
+      ];
+      //Plotly.purge('chart-geodemos'); 
+      Plotly.newPlot('chart-geodemos', data, hmlayout);
+
+})
+
 
 }
