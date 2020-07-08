574e15bfaf614078e28cd129db2983df9c6f60c7
LiamOSullivan <lmosulli@tcd.ie>
2020-07-08 11:41:30 +0100

    add route to geodemos query

--- routes/queries.js
+++ routes/queries.js
@@ -1,37 +1,20 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET queries page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('queries', {
     title: 'Queries Page',
     active: 'queries'
-  });
-});
+  })
+})
 
-router.get('/q1', function(req, res, next) {
-  //alert('you clicked q1')
-  res.render('queries/qt1', {
-    title: 'test',
+router.get('/geodemos', function (req, res, next) {
+  // alert('you clicked q1')
+  res.render('queries/geodemos', {
+    title: 'Query: Geodemographics',
     page: ''
-  });
-});
+  })
+})
 
-router.get('/q2', function(req, res, next) {
-  //alert('you clicked q2')
-  res.render('queries/qt2', {
-    title: '',
-    page: ''
-  });
-});
-
-
-router.get('/q3', function(req, res, next) {
-  //alert('you clicked q3')
-  res.render('queries/qt3', {
-    title: '',
-    page: ''
-  });
-});
-
-module.exports = router;
+module.exports = router
