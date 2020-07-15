e383fd263f97440b1e1cd427266d8adbd39033e4
LiamOSullivan <lmosulli@tcd.ie>
2020-07-15 11:47:48 +0100

    update rent chart to statbank

--- public/javascripts/themes/housing/housing_rppi.js
+++ public/javascripts/themes/housing/housing_rppi.js
@@ -6,28 +6,29 @@ import { MultiLineChart } from '../../modules/MultiLineChart.js'
 import { activeBtn } from '../../modules/bcd-ui.js'
 
 (async () => {
-  const parseYear = d3.timeParse('%Y')
-  const parseYearMonth = d3.timeParse('%YM%m') // ie 2014-Jan = Wed Jan 01 2014 00:00:00
-  const STATBANK_BASE_URL =
+  try {
+    const parseYear = d3.timeParse('%Y')
+    const parseYearMonth = d3.timeParse('%YM%m') // ie 2014-Jan = Wed Jan 01 2014 00:00:00
+    const STATBANK_BASE_URL =
           'https://statbank.cso.ie/StatbankServices/StatbankServices.svc/jsonservice/responseinstance/'
   // HPM05: Market-based Household Purchases of Residential Dwellings by Type of Dwelling, Dwelling Status, Stamp Duty Event, RPPI Region, Month and Statistic
-  const TABLE_CODE = 'HPM09' // gives no of outsideState and ave household size
-  let json = await fetchJsonFromUrlAsync(STATBANK_BASE_URL + TABLE_CODE)
-  let dataset = JSONstat(json).Dataset(0)
+    const TABLE_CODE = 'HPM09' // gives no of outsideState and ave household size
+    let json = await fetchJsonFromUrlAsync(STATBANK_BASE_URL + TABLE_CODE)
+    let dataset = JSONstat(json).Dataset(0)
 
-  let dimensions = dataset.Dimension().map(dim => {
-    return dim.label
-  })
+    let dimensions = dataset.Dimension().map(dim => {
+      return dim.label
+    })
 
-  let categoriesRegion = dataset.Dimension(dimensions[0]).Category().map(c => {
-    return c.label
-  })
+    let categoriesRegion = dataset.Dimension(dimensions[0]).Category().map(c => {
+      return c.label
+    })
   //
-  let categoriesStat = dataset.Dimension(dimensions[2]).Category().map(c => {
-    return c.label
-  })
+    let categoriesStat = dataset.Dimension(dimensions[2]).Category().map(c => {
+      return c.label
+    })
 
-  let houseRppiTable = dataset.toTable(
+    let houseRppiTable = dataset.toTable(
      { type: 'arrobj' },
      (d, i) => {
        if ((d[dimensions[0]] === categoriesRegion[4]
@@ -43,214 +44,33 @@ import { activeBtn } from '../../modules/bcd-ui.js'
        }
      })
 
-  let houseRppi = {
-    e: '#chart-house-rppi',
-    d: houseRppiTable.filter(d => {
-      return parseInt(d.date.getFullYear()) >= 2010
-    }),
-    ks: categoriesRegion,
-    k: dimensions[0],
-    xV: 'date',
-    yV: 'value',
-    tX: 'Year',
-    tY: ''
-  }
+    let houseRppi = {
+      e: '#chart-house-rppi',
+      d: houseRppiTable.filter(d => {
+        return parseInt(d.date.getFullYear()) >= 2010
+      }),
+      ks: categoriesRegion,
+      k: dimensions[0],
+      xV: 'date',
+      yV: 'value',
+      tX: 'Year',
+      tY: ''
+    }
 
-  let houseRppiChart = new MultiLineChart(houseRppi)
+    let houseRppiChart = new MultiLineChart(houseRppi)
 
-  function redraw () {
-    houseRppiChart.drawChart()
-    houseRppiChart.addTooltip('RPPI for ', '', 'label')
-  }
-  redraw()
-
-  window.addEventListener('resize', () => {
+    function redraw () {
+      houseRppiChart.drawChart()
+      houseRppiChart.addTooltip('RPPI for ', '', 'label')
+    }
     redraw()
-  })
+
+    window.addEventListener('resize', () => {
+      redraw()
+    })
+  } catch (e) {
+    console.log('Error creating RPPI chart')
+    console.log(e)
+  }
 })()
 
-// if (document.getElementById('chart-house-prices')) {
-//   // setup chart and data for quarterly house prices chart
-//   // process the data
-//   const housePriceMeansData = datafiles[4],
-//     housePriceMeansType = housePriceMeansData.columns.slice(2),
-//     housePriceMeansDate = housePriceMeansData.columns[0],
-//     housePriceMeansRegions = housePriceMeansData.columns[1],
-//     housePriceMeansDataProcessed = dataSets(housePriceMeansData, housePriceMeansType),
-//     yLabels4 = []
-//
-//   housePriceMeansDataProcessed.forEach(d => {
-//     d.label = (d[housePriceMeansDate])
-//     d[housePriceMeansDate] = convertQuarter(d[housePriceMeansDate])
-//   })
-//
-//   const housePriceMeansContent = {
-//     e: '#chart-house-prices',
-//     d: housePriceMeansDataProcessed,
-//     k: housePriceMeansRegions,
-//     xV: housePriceMeansDate,
-//     yV: 'value',
-//     tX: 'Quarters',
-//     tY: '€'
-//   }
-//
-//   // draw the chart
-//   housePriceMeansChart = new MultiLineChart(housePriceMeansContent)
-//
-//   function redraw () {
-//     housePriceMeansChart.ySF = 'millions'
-//     housePriceMeansChart.drawChart()
-//     housePriceMeansChart.addTooltip('In thousands - ', 'thousands', 'label', '€')
-//   }
-//   redraw()
-//
-//   window.addEventListener('resize', () => {
-//     redraw()
-//   })
-// }
-
-if (document.getElementById('chart-HPM06')) {
-  // new chart Price Index
-  //   const HPM06 = datafiles[11],
-  //     HPM06R = HPM06.columns[1],
-  //     HPM06V = HPM06.columns[2],
-  //     HPM06V2 = HPM06.columns[3],
-  //     HPM06V3 = HPM06.columns[3],
-  //     HPM06D = HPM06.columns[0]
-  //
-  // // create content object
-  //   const HPM06Content = chartContent(HPM06, HPM06R, HPM06V, HPM06D, '#chart-HPM06')
-  //   HPM06Content.tX = 'Months'
-  //   HPM06Content.tY = 'Price Index (Base 100)'
-  //
-  // // draw the chart
-  //   HPM06Charts = new MultiLineChart(HPM06Content)
-  //   function redraw () {
-  //     HPM06Charts.drawChart() // draw axis
-  //     HPM06Charts.addTooltip('Price Index - ', '', 'label') // add tooltip
-  //     HPM06Charts.addBaseLine(100) // add horizontal baseline
-  //   }
-  //   window.addEventListener('resize', () => {
-  //     redraw()
-  //   })
-}
-
-if (document.getElementById('chart-HPM06') && document.getElementById('chart-house-prices')) {
-    // d3.select('#chart-house-prices').style('display', 'block')
-    // d3.select('#chart-HPM06').style('display', 'none')
-    //
-    // d3.select('#btn-house-prices').on('click', function () {
-    //   activeBtn(this)
-    //   d3.select('#chart-house-prices').style('display', 'block')
-    //   d3.select('#chart-HPM06').style('display', 'none')
-    //   housePriceMeansChart.drawChart()
-    //   housePriceMeansChart.addTooltip('In thousands - ', 'thousands', 'label', '€')
-    // })
-    //
-    // d3.select('#btn-HPM06').on('click', function () {
-    //   activeBtn(this)
-    //
-    //   d3.select('#chart-house-prices').style('display', 'none')
-    //   d3.select('#chart-HPM06').style('display', 'block')
-    //   HPM06Charts.drawChart() // draw axis
-    //   HPM06Charts.addTooltip('Price Index - ', '', 'label') // add tooltip
-    //   HPM06Charts.addBaseLine(100) // add horizontal baseline
-    // })
-    //
-    // // window.addEventListener('resize', () => {
-    // //   redraw()
-    // // })
-}
-
-function convertQuarter (q) {
-  let splitted = q.split('Q')
-  let year = splitted[0]
-  let quarterEndMonth = splitted[1] * 3 - 2
-  let date = d3.timeParse('%m %Y')(quarterEndMonth + ' ' + year)
-  return date
-}
-
-function qToQuarter (q) {
-  let splitted = q.split('Q')
-  let year = splitted[0]
-  let quarter = splitted[1]
-  let quarterString = ('Quarter ' + quarter + ' ' + year)
-  return quarterString
-}
-
-function dataSets (data, columns) {
-  let coercedData = data.map(d => {
-    for (var i = 0, n = columns.length; i < n; i++) {
-      // d[columns[i]] !== "null" ? d[columns[i]] = +d[columns[i]] : d[columns[i]] = "unavailable";
-      d[columns[i]] = +d[columns[i]]
-    }
-    return d
-  })
-  return coercedData
-}
-
-function formatQuarter (date) {
-  let newDate = new Date()
-  newDate.setMonth(date.getMonth() + 1)
-  let year = (date.getFullYear())
-  let q = Math.ceil((newDate.getMonth()) / 3)
-  return 'Quarter ' + q + ' ' + year
-}
-
-function filterbyDate (data, dateField, date) {
-  return data.filter(d => {
-    return d[dateField] >= new Date(date)
-  })
-}
-
-function filterByDateRange (data, dateField, dateOne, dateTwo) {
-  return data.filter(d => {
-    return d[dateField] >= new Date(dateOne) && d[dateField] <= new Date(dateTwo)
-  })
-}
-
-function nestData (data, label, name, value) {
-  let nested_data = d3.nest()
-    .key(function (d) {
-      return d[label]
-    })
-    .entries(data) // its the string not the date obj
-
-  let mqpdata = nested_data.map(function (d) {
-    let obj = {
-      label: d.key
-    }
-    d.values.forEach(function (v) {
-      obj[v[name]] = v[value]
-      obj.date = v.date
-    })
-    return obj
-  })
-  return mqpdata
-}
-
-function chartContent (data, key, value, date, selector) {
-  data.forEach(function (d) { // could pass types array and coerce each matching key using dataSets()
-    d.label = d[date]
-    d.date = parseYearMonth(d[date])
-    d[value] = +d[value]
-  })
-
-  // nest the processed data by regions
-  const nest = d3.nest().key(d => {
-    return d[key]
-  }).entries(data)
-
-  // get array of keys from nest
-  const keys = []
-  nest.forEach(d => {
-    keys.push(d.key)
-  })
-
-  return {
-    e: selector,
-    d: nest,
-    xV: date,
-    yV: value
-  }
-}
