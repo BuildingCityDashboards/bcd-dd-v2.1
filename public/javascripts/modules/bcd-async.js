69724c58decec53a4c952c3c1fb5b67db8e182bb
LiamOSullivan <lmosulli@tcd.ie>
2020-07-16 21:20:09 +0100

    add async timeout to module to adress #1270, #791

--- public/javascripts/modules/bcd-async.js
+++ public/javascripts/modules/bcd-async.js
@@ -1,10 +1,21 @@
 const fetchJsonFromUrlAsync = async (url) => {
+  console.log('fetch w/o timeout')
   const res = await fetch(url)
   const json = await res.json()
   return json
 }
 
 export { fetchJsonFromUrlAsync }
+
+const fetchJsonFromUrlAsyncTimeout = async (url) => {
+  let res = await Promise.race([fetch(url), new Promise((resolve, reject) =>
+      setTimeout(() => reject(new Error(`Timeout waiting for ${url} to respond`)), 10000)
+    )])
+  const json = await res.json()
+  return json
+}
+
+export { fetchJsonFromUrlAsyncTimeout }
 
 const forEachAsync = async (array, callback) => {
   for (let index = 0; index < array.length; index++) {
