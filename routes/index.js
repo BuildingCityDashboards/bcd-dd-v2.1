b545636cffb719496eef96a55231d2f0d293f11d
LiamOSullivan <lmosulli@tcd.ie>
2020-08-05 15:34:11 +0100

    add route for domain verification string

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,31 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-var authorities_controller = require('../controllers/authoritiesController');
-var qnq22_controller = require('../controllers/QNQ22Controller');
+var authorities_controller = require('../controllers/authoritiesController')
+var qnq22_controller = require('../controllers/QNQ22Controller')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Dublin Dashboard', page:'page dark', active:'home' });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Dublin Dashboard', page: 'page dark', active: 'home' })
+})
+
+router.get('/.well-known/pki-validation/godaddy.html', function (req, res, next) {
+  res.send('3htnrec99acmov7afigq82f6vj')
+})
 
 // GET request for list of all Local Authorities Documents.
-router.get('/authorities', authorities_controller.authorities_list);
+router.get('/authorities', authorities_controller.authorities_list)
 
 // GET request for list of all County Specific Documents.
-router.get('/authorities/:id', authorities_controller.authorities_county);
+router.get('/authorities/:id', authorities_controller.authorities_county)
 
 // GET request for list of all QNQ22 Documents.
-router.get('/qnq22', qnq22_controller.QNQ22_list);
+router.get('/qnq22', qnq22_controller.QNQ22_list)
 
 // GET request for list of all QNQ22 Documents by Region.
-router.get('/qnq22/unemployment', qnq22_controller.QNQ22_unemployment);
+router.get('/qnq22/unemployment', qnq22_controller.QNQ22_unemployment)
 
 // GET request for list of all QNQ22 Documents by Region.
-router.get('/qnq22/employment', qnq22_controller.QNQ22_employment);
+router.get('/qnq22/employment', qnq22_controller.QNQ22_employment)
 
-module.exports = router;
+module.exports = router
