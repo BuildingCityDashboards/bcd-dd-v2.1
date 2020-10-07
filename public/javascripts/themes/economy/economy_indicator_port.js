f3c028705a769f009d3145e450fc8863ddad02c7
LiamOSullivan <lmosulli@tcd.ie>
2020-10-07 12:58:39 +0100

    fix for new DEM data format- Port data

--- public/javascripts/themes/economy/economy_indicator_port.js
+++ public/javascripts/themes/economy/economy_indicator_port.js
@@ -4,94 +4,94 @@ import { activeBtn } from '../../modules/bcd-ui.js'
 import { MultiLineChart } from '../../modules/MultiLineChart.js'
 import { StackedAreaChart } from '../../modules/StackedAreaChart.js'
 let portTotalChart, portBreakdownChart
-let portTonnage = '../data/Economy/data_gov_economic_monitor/indicator-10-dublin-port-tonnage.csv'
+const portTonnage = '../data/Economy/data_gov_economic_monitor/indicator-10-dublin-port-tonnage.csv'
 
 Promise.all([
   d3.csv(portTonnage)
 ])
-.then(data => {
-  let portData = data[0]
-  let portColumns = portData.columns.slice(1)
-  if (document.getElementById('chart-indicator-port-total')) {
-    let longData = portData.map(d => {
+  .then(data => {
+    const portData = data[0]
+    const portColumns = portData.columns.slice(1)
+    if (document.getElementById('chart-indicator-port-total')) {
+      const longData = portData.map(d => {
       // the date is re-formatted  "'Q'Q YY" -> "YYYY'Q'Q"
-      let yearQuarter = '20' + d.Quarter.toString().split(' ')[1] + d.Quarter.toString().split(' ')[0]
-      let obj = {
+        const yearQuarter = '20' + d.Quarter.toString().split(' ')[1] + d.Quarter.toString().split(' ')[0]
+        const obj = {
 
-        value: parseFloat(d[portColumns[0]].replace(/,/g, '')) / 1000000,
-        variable: portColumns[0],
-        date: convertQuarterToDate(yearQuarter),
-        label: yearQuarter.replace(/Q/, ' Q')
+          value: parseFloat(d[portColumns[0]].replace(/,/g, '')) / 1000000,
+          variable: portColumns[0],
+          date: convertQuarterToDate(yearQuarter),
+          label: yearQuarter.replace(/Q/, ' Q')
+        }
+        return obj
+      })
+
+      const portTonnageCount = {
+        e: '#chart-indicator-port-total',
+        d: longData,
+        k: 'variable', // key whose value will name the traces (group by)
+        xV: 'date',
+        yV: 'value',
+        tX: 'Quarter',
+        tY: 'Tonnes (millions)'
       }
-      return obj
-    })
-
-    const portTonnageCount = {
-      e: '#chart-indicator-port-total',
-      d: longData,
-      k: 'variable', // key whose value will name the traces (group by)
-      xV: 'date',
-      yV: 'value',
-      tX: 'Quarter',
-      tY: 'Tonnes (millions)'
-    }
-    portTotalChart = new MultiLineChart(portTonnageCount)
-    function redraw () {
-      portTotalChart.drawChart()
-      portTotalChart.addTooltip('Total tonnage through port, ', 'thousands', 'label')
-    }
-    redraw()
-
-    window.addEventListener('resize', () => {
+      portTotalChart = new MultiLineChart(portTonnageCount)
+      function redraw () {
+        portTotalChart.drawChart()
+        portTotalChart.addTooltip('Total tonnage through port, ', 'thousands', 'label')
+      }
       redraw()
-    })
-  }
 
-  if (document.getElementById('chart-indicator-port-breakdown')) {
-    let breakdownCols = portColumns.slice(2, 4)
-
-    let portBreakdownData = portData.map(d => {
-      let yearQuarter = '20' + d.Quarter.toString().split(' ')[1] + d.Quarter.toString().split(' ')[0]
-      d.label = yearQuarter.replace(/Q/, ' Q')
-      d.date = convertQuarterToDate(yearQuarter)
-      for (var i = 0, n = breakdownCols.length; i < n; i++) {
-        d[breakdownCols[i]] = parseFloat(d[breakdownCols[i]].replace(/,/g, '')) / 1000000
-      }
-      return d
-    }).filter(d => {
-      return !Number.isNaN(d.Imports)
-    })
-
-    const portTonnageBreakdown = {
-      e: '#chart-indicator-port-breakdown',
-      d: portBreakdownData,
-      ks: breakdownCols,
-      xV: 'date',
-      yV: breakdownCols,
-      tX: 'Quarter',
-      tY: 'Tonnes (millions)'
+      window.addEventListener('resize', () => {
+        redraw()
+      })
     }
-    portBreakdownChart = new StackedAreaChart(portTonnageBreakdown)
-    portBreakdownChart.drawChart()
-    portBreakdownChart.addTooltip('Total tonnage through port, ', 'thousands', 'label')
-  }
 
-  d3.select('#chart-indicator-port-total').style('display', 'block')
-  d3.select('#chart-indicator-port-breakdown').style('display', 'none')
+    if (document.getElementById('chart-indicator-port-breakdown')) {
+      const breakdownCols = portColumns.slice(10, 12)
 
-  d3.select('#btn-indicator-port-total').on('click', function () {
-    activeBtn(this)
+      const portBreakdownData = portData.map(d => {
+        const yearQuarter = '20' + d.Quarter.toString().split(' ')[1] + d.Quarter.toString().split(' ')[0]
+        d.label = yearQuarter.replace(/Q/, ' Q')
+        d.date = convertQuarterToDate(yearQuarter)
+        for (var i = 0, n = breakdownCols.length; i < n; i++) {
+          d[breakdownCols[i]] = parseFloat(d[breakdownCols[i]].replace(/,/g, '')) / 1000000
+        }
+        return d
+      }).filter(d => {
+        return !Number.isNaN(d.Imports) && d.Imports !== 0
+      })
+
+      const portTonnageBreakdown = {
+        e: '#chart-indicator-port-breakdown',
+        d: portBreakdownData,
+        ks: breakdownCols,
+        xV: 'date',
+        yV: breakdownCols,
+        tX: 'Quarter',
+        tY: 'Tonnes (millions)'
+      }
+      portBreakdownChart = new StackedAreaChart(portTonnageBreakdown)
+      portBreakdownChart.drawChart()
+      portBreakdownChart.addTooltip('Total tonnage through port, ', 'thousands', 'label')
+    }
+
     d3.select('#chart-indicator-port-total').style('display', 'block')
     d3.select('#chart-indicator-port-breakdown').style('display', 'none')
-    portTotalChart.drawChart()
-    portTotalChart.addTooltip('Tonnage, ', 'thousands', 'label')
-  })
 
-  d3.select('#btn-indicator-port-breakdown').on('click', function () {
-    activeBtn(this)
-    d3.select('#chart-indicator-port-total').style('display', 'none')
-    d3.select('#chart-indicator-port-breakdown').style('display', 'block')
-    portBreakdownChart.drawChart()
-    portBreakdownChart.addTooltip('Tonnage, ', 'thousands', 'label')
+    d3.select('#btn-indicator-port-total').on('click', function () {
+      activeBtn(this)
+      d3.select('#chart-indicator-port-total').style('display', 'block')
+      d3.select('#chart-indicator-port-breakdown').style('display', 'none')
+      portTotalChart.drawChart()
+      portTotalChart.addTooltip('Tonnage, ', 'thousands', 'label')
+    })
+
+    d3.select('#btn-indicator-port-breakdown').on('click', function () {
+      activeBtn(this)
+      d3.select('#chart-indicator-port-total').style('display', 'none')
+      d3.select('#chart-indicator-port-breakdown').style('display', 'block')
+      portBreakdownChart.drawChart()
+      portBreakdownChart.addTooltip('Tonnage, ', 'thousands', 'label')
+    })
   })
-})
