bd942b528e901de7a6362c342f7d140d0d2c6d3e
LiamOSullivan <lmosulli@tcd.ie>
2020-10-21 20:40:23 +0100

    update born-outside-state chart to new style, add spinner and error retry

--- public/javascripts/themes/demographics/demographics_born_outside_state.js
+++ public/javascripts/themes/demographics/demographics_born_outside_state.js
@@ -1,144 +1,151 @@
 import { fetchJsonFromUrlAsync } from '../../modules/bcd-async.js'
-import { convertQuarterToDate } from '../../modules/bcd-date.js'
-import { stackNest } from '../../modules/bcd-data.js'
 import JSONstat from 'https://unpkg.com/jsonstat-toolkit@1.0.8/import.mjs'
-import { MultiLineChart } from '../../modules/MultiLineChart.js'
-import { StackedAreaChart } from '../../modules/StackedAreaChart.js'
+import { BCDMultiLineChart } from '../../modules/BCDMultiLineChart.js'
 import { activeBtn } from '../../modules/bcd-ui.js'
+import { TimeoutError } from '../../modules/TimeoutError.js'
+import { addSpinner, removeSpinner, addErrorMessageButton, removeErrorMessageButton } from '../../modules/bcd-ui.js'
 
-(async () => {
-  const parseYear = d3.timeParse('%Y')
-  const STATBANK_BASE_URL =
-    'https://statbank.cso.ie/StatbankServices/StatbankServices.svc/jsonservice/responseinstance/'
-  // CNA31: Populaton by Country of Birth, County and Year
-  const TABLE_CODE = 'CNA31' // gives no of outsideState and ave household size
-  const json = await fetchJsonFromUrlAsync(STATBANK_BASE_URL + TABLE_CODE)
-
-  const dataset = JSONstat(json).Dataset(0)
-  // console.log('dataset')
-  // console.log(dataset)
-  // console.log('dim')
-  const dimensions = dataset.Dimension().map(dim => {
-    return dim.label
-  })
-  // console.log(dimensions)
-
-  const categoriesOfCountry = dataset.Dimension(dimensions[0]).Category().map(c => {
-    return c.label
-  })
-
-  // console.log('categories of country')
-  // console.log(categoriesOfCountry)
-
-  // let STATS = ['Population (Number)']
-  // = ['Total Birth', 'Great Britain', 'U.S.A.', 'Other Countries']
-
-  const traceNames = []
-  //
-  const bornOutsideTable = dataset.toTable(
-    { type: 'arrobj' },
-    (d, i) => {
-      if (
-        // d[DIMENSION] === 'State'||
-        // (d[dimensions[0]] === categoriesOfCountry[0] ||
-        (d[dimensions[0]] === categoriesOfCountry[0] ||
-          d[dimensions[0]] === categoriesOfCountry[5] ||
-          d[dimensions[0]] === categoriesOfCountry[8] ||
-          d[dimensions[0]] === categoriesOfCountry[9]) &&
-        (d.County === 'Dublin' ||
-          d.County === 'State') &&
-        +d.Year >= 1991) {
-        d.date = parseYear(+d.Year)
-        d.label = +d.Year
-        d.value = +d.value / 1000
-        if (!traceNames.includes(d[dimensions[0]])) {
-          traceNames.push(d[dimensions[0]])
-        }
-        return d
-      }
-    })
-
-  // console.log(bornOutsideTable)
-
-  const bornOutsideDublin = {
-    e: '#chart-born-outside-dublin',
-    d: bornOutsideTable.filter(d => {
-      return d.County === 'Dublin'
-    }),
-    ks: traceNames,
-    k: dimensions[0],
-    xV: 'date',
-    yV: 'value',
-    tX: 'Year',
-    tY: 'Population (thousands)'
-  }
-  //
-  const bornOutsideDublinChart = new BCDMultiLineChart(bornOutsideDublin)
-
-  // bornOutsideDublinChart.tickNumber = 31
-  bornOutsideDublinChart.drawChart()
-  // bornOutsideDublinChart.addTooltip(outsideStateTT)
-  bornOutsideDublinChart.addTooltip('Population (thousands) in census year ', '', 'label')
-  // bornOutsideDublinChart.showSelectedLabels([1, 6, 11, 17, 21, 26, 31])
-  //
-
-  const bornOutsideState = {
-    e: '#chart-born-outside-state',
-    d: bornOutsideTable.filter(d => {
-      return d.County === 'State'
-    }),
-    ks: traceNames,
-    k: dimensions[0],
-    xV: 'date',
-    yV: 'value',
-    tX: 'Year',
-    tY: 'Population (thousands)'
-  }
-
-  const bornOutsideStateChart = new BCDMultiLineChart(bornOutsideState)
-  bornOutsideStateChart.drawChart()
-  bornOutsideStateChart.addTooltip('Population (thousands) in census year ', '', 'label')
-
+(async function main () {
   const chart1 = 'born-outside-dublin'
   const chart2 = 'born-outside-state'
 
   d3.select('#chart-' + chart1).style('display', 'block')
   d3.select('#chart-' + chart2).style('display', 'none')
+  const parseYear = d3.timeParse('%Y')
+  const STATBANK_BASE_URL =
+    'https://statbank.cso.ie/StatbankServices/StatbankServices.svc/jsonservice/responseinstance/'
+  // CNA31: Populaton by Country of Birth, County and Year
+  const TABLE_CODE = 'CNA31' // gives no of outsideState and ave household size
 
-  d3.select('#btn-' + chart1).on('click', function () {
-    activeBtn(this)
-    d3.select('#chart-' + chart1).style('display', 'block')
-    d3.select('#chart-' + chart2).style('display', 'none')
-    // bornOutsideDublinChart.tickNumber = 31
-    bornOutsideDublinChart.drawChart()
-    bornOutsideDublinChart.addTooltip('Population (thousands) in census year ', '', 'label')
-    // bornOutsideDublinChart.addTooltip(outsideStateTT)
-    // bornOutsideDublinChart.addTooltip(STATS[0].split('(')[0], '', 'label')
-    // bornOutsideDublinChart.showSelectedLabels([1, 6, 11, 17, 21, 26, 31])
-  })
-
-  d3.select('#btn-' + chart2).on('click', function () {
-    activeBtn(this)
-    d3.select('#chart-' + chart1).style('display', 'none')
-    d3.select('#chart-' + chart2).style('display', 'block')
-    bornOutsideStateChart.drawChart()
-    bornOutsideStateChart.addTooltip('Population (thousands) in census year ', '', 'label')
-  })
-
-  window.addEventListener('resize', () => {
-    // console.log('redraw outsideState')
-    // bornOutsideDublinChart.tickNumber = 31
-    if (document.querySelector('#chart-' + chart1).style.display !== 'none') {
-      bornOutsideDublinChart.drawChart()
-      bornOutsideDublinChart.addTooltip('Population (thousands) in census year ', '', 'label')
+  try {
+    addSpinner('chart-' + chart1, '<b>CSO</b> for data: <i>Populaton by Country of Birth, County and Year</i>')
+    const json = await fetchJsonFromUrlAsync(STATBANK_BASE_URL + TABLE_CODE)
+    if (json) {
+      removeSpinner('chart-' + chart1)
     }
-    if (document.querySelector('#chart-' + chart2).style.display !== 'none') {
-      bornOutsideStateChart.drawChart()
-      bornOutsideStateChart.addTooltip('Population (thousands) in census year ', '', 'label')
+
+    const dataset = JSONstat(json).Dataset(0)
+    // console.log('dataset')
+    // console.log(dataset)
+    // console.log('dim')
+    const dimensions = dataset.Dimension().map(dim => {
+      return dim.label
+    })
+    // console.log(dimensions)
+
+    const categoriesOfCountry = dataset.Dimension(dimensions[0]).Category().map(c => {
+      return c.label
+    })
+
+    // console.log('categories of country')
+    // console.log(categoriesOfCountry)
+
+    // let STATS = ['Population (Number)']
+    // = ['Total Birth', 'Great Britain', 'U.S.A.', 'Other Countries']
+
+    const traceNames = []
+    //
+    const bornOutsideTable = dataset.toTable(
+      { type: 'arrobj' },
+      (d, i) => {
+        if (
+          // d[DIMENSION] === 'State'||
+          // (d[dimensions[0]] === categoriesOfCountry[0] ||
+          (d[dimensions[0]] === categoriesOfCountry[0] ||
+            d[dimensions[0]] === categoriesOfCountry[5] ||
+            d[dimensions[0]] === categoriesOfCountry[8] ||
+            d[dimensions[0]] === categoriesOfCountry[9]) &&
+          (d.County === 'Dublin' ||
+            d.County === 'State') &&
+          +d.Year >= 1991) {
+          d.date = parseYear(+d.Year)
+          d.label = +d.Year
+          d.value = +d.value 
+          if (!traceNames.includes(d[dimensions[0]])) {
+            traceNames.push(d[dimensions[0]])
+          }
+          return d
+        }
+      })
+
+    // console.log(bornOutsideTable)
+
+    const bornOutsideDublin = {
+      e: 'chart-born-outside-dublin',
+      d: bornOutsideTable.filter(d => {
+        return d.County === 'Dublin'
+      }),
+      ks: traceNames,
+      k: dimensions[0],
+      xV: 'date',
+      yV: 'value',
+      tX: 'Year',
+      tY: 'Population',
+      formaty: 'hundredThousandsShort'
     }
-    // bornOutsideDublinChart.addTooltip(outsideStateTT)
-    // bornOutsideDublinChart.addTooltip('Population (thousands) in census year ', '', 'label')
-    // bornOutsideStateChart.addTooltip('Population (thousands) in census year ', '', 'label')
-    // bornOutsideDublinChart.showSelectedLabels([1, 6, 11, 17, 21, 26, 31])
-  })
+    //
+    const bornOutsideDublinChart = new BCDMultiLineChart(bornOutsideDublin)
+
+    redraw(bornOutsideDublinChart)
+
+    const bornOutsideState = {
+      e: 'chart-born-outside-state',
+      d: bornOutsideTable.filter(d => {
+        return d.County === 'State'
+      }),
+      ks: traceNames,
+      k: dimensions[0],
+      xV: 'date',
+      yV: 'value',
+      tX: 'Year',
+      tY: 'Population',
+      formaty: 'hundredThousandsShort'
+    }
+
+    const bornOutsideStateChart = new BCDMultiLineChart(bornOutsideState)
+    redraw(bornOutsideStateChart)
+
+    d3.select('#btn-' + chart1).on('click', function () {
+      activeBtn(this)
+      d3.select('#chart-' + chart1).style('display', 'block')
+      d3.select('#chart-' + chart2).style('display', 'none')
+      redraw(bornOutsideDublinChart)
+    })
+
+    d3.select('#btn-' + chart2).on('click', function () {
+      activeBtn(this)
+      d3.select('#chart-' + chart1).style('display', 'none')
+      d3.select('#chart-' + chart2).style('display', 'block')
+      redraw(bornOutsideStateChart)
+    })
+
+    window.addEventListener('resize', () => {
+      // console.log('redraw outsideState')
+      // bornOutsideDublinChart.tickNumber = 31
+      if (document.querySelector('#chart-' + chart1).style.display !== 'none') {
+        redraw(bornOutsideDublinChart)
+      }
+      if (document.querySelector('#chart-' + chart2).style.display !== 'none') {
+        redraw(bornOutsideStateChart)
+      }
+    })
+  } catch (e) {
+    console.log('Error creating demographics chart')
+    console.log(e)
+    removeSpinner('chart-' + chart1)
+    const eMsg = e instanceof TimeoutError ? e : 'An error occured'
+    const errBtnID = addErrorMessageButton('chart-' + chart1, eMsg)
+    // console.log(errBtnID)
+    d3.select(`#${errBtnID}`).on('click', function () {
+      removeErrorMessageButton('chart-' + chart1)
+      main()
+    })
+  }
 })()
+
+function redraw (chart) {
+  chart.drawChart()
+  chart.addTooltip('Population (thousands) in census year ', '', 'label')
+  chart.showSelectedLabelsX([0, 2, 4, 6, 8, 10])
+  chart.showSelectedLabelsY([2, 4, 6, 8, 10])
+}
