625397487bf9fb50a44568135d2050545811b0e3
LiamOSullivan <lmosulli@tcd.ie>
2020-10-21 18:54:47 +0100

    update airport pax chart to new style, add spinner and error retry

--- public/javascripts/themes/economy/economy_indicator_airport.js
+++ public/javascripts/themes/economy/economy_indicator_airport.js
@@ -1,46 +1,69 @@
 import { convertQuarterToDate } from '../../modules/bcd-date.js'
-import { coerceWideTable } from '../../modules/bcd-data.js'
-import { MultiLineChart } from '../../modules/MultiLineChart.js'
-let airportPaxCountChart, airportPaxRateChart
-const airportPax = '../data/Economy/data_gov_economic_monitor/indicator-.9.-dublin-airport-pax.csv'
+// import { coerceWideTable } from '../../modules/bcd-data.js'
+import { BCDMultiLineChart } from '../../modules/BCDMultiLineChart.js'
+import { addSpinner, removeSpinner, addErrorMessageButton, removeErrorMessageButton } from '../../modules/bcd-ui.js'
+import { TimeoutError } from '../../modules/TimeoutError.js'
+import { fetchCsvFromUrlAsyncTimeout } from '../../modules/bcd-async.js'
 
-Promise.all([
-  d3.csv(airportPax)
-])
-  .then(data => {
-    const airportData = data[0]
-    const airportColumns = airportData.columns.slice(1)
-    // console.log('airport')
-    // console.log(airportData)
+(async function main () {
+  let airportPaxCountChart
+  const airportPax = '../data/Economy/data_gov_economic_monitor/indicator-.9.-dublin-airport-pax.csv'
+
+  try {
+    addSpinner('chart-indicator-airport-count', '<b>data.gov.ie</b> for data: <i>Dublin Economic Monitor, Indicator 9 - Dublin Airport Passengers</i>')
+    let data = await fetchCsvFromUrlAsyncTimeout(airportPax)
+    data = d3.csvParse(data)
+    if (data) {
+      removeSpinner('chart-indicator-airport-count')
+    }
+    const airportColumns = data.columns.slice(1)
     if (document.getElementById('chart-indicator-airport-count')) {
-      const longData = airportData.map(d => {
+      const longData = data.map(d => {
         const yearQuarter = '20' + d.Quarter.toString().split(' ')[1] + d.Quarter.toString().split(' ')[0]
         const obj = {
-          label: yearQuarter.replace(/Q/g, ' qurater '),
+          label: yearQuarter.replace(/Q/g, ' Q'),
           value: (parseInt(d[airportColumns[0]].replace(/,/g, '')) / 1000000).toFixed(2),
           variable: airportColumns[0],
           date: convertQuarterToDate(yearQuarter)
         }
         return obj
       })
-
       const airportPaxCount = {
-        e: '#chart-indicator-airport-count',
+        e: 'chart-indicator-airport-count',
         d: longData,
         k: 'variable', // give the key whose value will name the traces (group by)
         xV: 'date',
         yV: 'value',
         tX: 'Quarter',
-        tY: 'Passengers (millions)'
+        tY: 'Passengers (millions)',
+        margins: {
+          left: 40
+        }
       }
       airportPaxCountChart = new BCDMultiLineChart(airportPaxCount)
-      function redraw() {
-        airportPaxCountChart.drawChart()
-        airportPaxCountChart.addTooltip('Millions of passengers, ', '', 'label')
-      }
-      redraw()
+
+      redraw(airportPaxCountChart)
       window.addEventListener('resize', () => {
-        redraw()
+        redraw(airportPaxCountChart)
       })
     }
-  })
+  } catch (e) {
+    console.log('Error creating Dublin Airport Passengers chart')
+    console.log(e)
+    removeSpinner('chart-indicator-airport-count')
+    const eMsg = e instanceof TimeoutError ? e : 'An error occured'
+    const errBtnID = addErrorMessageButton('chart-indicator-airport-count', eMsg)
+    // console.log(errBtnID)
+    d3.select(`#${errBtnID}`).on('click', function () {
+      removeErrorMessageButton('chart-indicator-airport-count')
+      main()
+    })
+  }
+})()
+
+function redraw (chart) {
+  chart.drawChart()
+  chart.addTooltip('Millions of passengers, ', '', 'label')
+  chart.showSelectedLabelsX([0, 2, 4, 6, 8, 10])
+  chart.showSelectedLabelsY([2, 4, 6, 8, 10])
+}
