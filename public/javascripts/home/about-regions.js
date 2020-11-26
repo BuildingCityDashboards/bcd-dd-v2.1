aa672255aa21c823bf882d8812092d988068e050
LiamOSullivan <lmosulli@tcd.ie>
2020-11-26 17:21:24 +0000

    fix 673

--- public/javascripts/home/about-regions.js
+++ public/javascripts/home/about-regions.js
@@ -1,31 +1,31 @@
 Promise.all([d3.xml('/images/home/dublin-regions-map.svg'),
   d3.json('/data/home/dublin-region-data.json')])
   .then(files => {
-    let xml = files[0]
-    let dublinRegionsJson = files[1]
+    const xml = files[0]
+    const dublinRegionsJson = files[1]
     // "xml" is the XML DOM tree
-    let htmlSVG = document.getElementById('map') // the svg-element in our HTML file
+    const htmlSVG = document.getElementById('map') // the svg-element in our HTML file
     // append the "maproot" group to the svg-element in our HTML file
-    htmlSVG.appendChild(xml.documentElement.getElementById('Dublin-ZoneMap'))
+    htmlSVG.appendChild(xml.getElementById('Dublin-ZoneMap'))
 
-    // d3 objects for later use
-    let dublinSvg = d3.select(htmlSVG)
-    let dccRoot = dublinSvg.select('#Dublin_City')
-    let dlrRoot = dublinSvg.select('#Dun_Laoghaire_Rathdown')
-    let sdRoot = dublinSvg.select('#South_Dublin')
-    let fRoot = dublinSvg.select('#Fingal')
+    // // d3 objects for later use
+    const dublinSvg = d3.select(htmlSVG)
+    const dccRoot = dublinSvg.select('#Dublin_City')
+    const dlrRoot = dublinSvg.select('#Dun_Laoghaire_Rathdown')
+    const sdRoot = dublinSvg.select('#South_Dublin')
+    const fRoot = dublinSvg.select('#Fingal')
 
-    // get the svg-element from the original SVG file
+    // // get the svg-element from the original SVG file
     const xmlSVG = d3.select(xml.getElementsByTagName('svg')[0])
-    // copy its "viewBox" attribute to the svg element in our HTML file
+    // // copy its "viewBox" attribute to the svg element in our HTML file
     dublinSvg.attr('viewBox', xmlSVG.attr('viewBox'))
 
-    let dccPath = dccRoot.select('path')
-    let dlrPath = dlrRoot.select('path')
-    let sdPath = sdRoot.select('path')
-    let fPath = fRoot.select('path')
+    const dccPath = dccRoot.select('path')
+    const dlrPath = dlrRoot.select('path')
+    const sdPath = sdRoot.select('path')
+    const fPath = fRoot.select('path')
 
-    let paths = [dccPath, dlrPath, sdPath, fPath]
+    const paths = [dccPath, dlrPath, sdPath, fPath]
     paths.forEach(p => {
       p.on('mouseover', function () {
         d3.select(this).style('fill', '#6fc6f6')
@@ -41,14 +41,14 @@ Promise.all([d3.xml('/images/home/dublin-regions-map.svg'),
         // let e = document.getElementById('about-dublin__card')
         // e.scrollIntoView()
 
-        let ref = d3.select(this.parentNode).attr('data-name')
-        //alert(ref)
+        const ref = d3.select(this.parentNode).attr('data-name')
+        // alert(ref)
         updateInfoText(dublinRegionsJson[ref])
         // on click, remove the call to action
         d3.select('#regions-info__cta').style('display', 'none')
         d3.select('#regions-info__cta-arrow').style('display', 'none')
 
-// This animated transition doesn't work
+        // This animated transition doesn't work
 
         d3.select('#regions-info__card').style('display', 'flex')
 
@@ -77,7 +77,7 @@ function updateInfoText (d) {
   d3.select('#local__prePopulation').html(popFormat(d.PREVPOPULATION) + '')
   d3.select('#local__curPopulation').html(popFormat(d.POPULATION) + '')
 
-  let change = getPerChange(d.POPULATION, d.PREVPOPULATION)
+  const change = getPerChange(d.POPULATION, d.PREVPOPULATION)
 
   d3.select('#local__populationIndicator').html(indicatorText(change, '#local__populationIndicator', 'increased', false))
   d3.select('#local__populationChange').html(percentage(change) + indicator_f(change, '#local__populationChange', false))
